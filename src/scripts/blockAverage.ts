b0b028ce1a97421be1b93d8bee259a9d888ab521
Bernardo Garces Chapero <bernardo.chapero@consensys.net>
2023-09-01 13:53:44 +0100

    filter

--- src/scripts/blockAverage.ts
+++ src/scripts/blockAverage.ts
@@ -10,34 +10,39 @@ export function blockAverage(program: Command) {
     .showHelpAfterError()
     .action(async ({ chainFilter }) => {
       const averageBlocksPerDay = await Promise.all(
-        Object.values(Chain).map(async (chainId) => {
-          const provider = chainProviders[chainId]
-
-          if (!provider) {
-            throw new Error('No provider for chain')
-          }
-
-          const dater = new EthDater(provider)
-
-          const currentBlock = await provider.getBlock('latest')
-
-          // 30 days * 24 hours/day * 60 minutes/hour * 60 seconds/minute
-          const thirtyDaysTimestamp = currentBlock.timestamp - 30 * 24 * 60 * 60
-
-          // Value needs to be given in milliseconds, thus the * 1000
-          const thirtyDaysBlock = await dater.getDate(
-            thirtyDaysTimestamp * 1000,
+        Object.values(Chain)
+          .filter(
+            (chainId) => !chainFilter || chainId === parseInt(chainFilter, 10),
           )
+          .map(async (chainId) => {
+            const provider = chainProviders[chainId]
 
-          const averageBlocksPerDay = Math.round(
-            (currentBlock.number - thirtyDaysBlock.block) / 30,
-          )
+            if (!provider) {
+              throw new Error('No provider for chain')
+            }
 
-          return {
-            chainId,
-            averageBlocksPerDay,
-          }
-        }),
+            const dater = new EthDater(provider)
+
+            const currentBlock = await provider.getBlock('latest')
+
+            // 30 days * 24 hours/day * 60 minutes/hour * 60 seconds/minute
+            const thirtyDaysTimestamp =
+              currentBlock.timestamp - 30 * 24 * 60 * 60
+
+            // Value needs to be given in milliseconds, thus the * 1000
+            const thirtyDaysBlock = await dater.getDate(
+              thirtyDaysTimestamp * 1000,
+            )
+
+            const averageBlocksPerDay = Math.round(
+              (currentBlock.number - thirtyDaysBlock.block) / 30,
+            )
+
+            return {
+              chainId,
+              averageBlocksPerDay,
+            }
+          }),
       )
 
       console.log(averageBlocksPerDay)
