48e4383e2b0cc94e7a2eb604f217af1f25bf9ecc
Bernardo Garces Chapero <bernardo.chapero@consensys.net>
2023-10-18 15:51:37 +0100

    snapshots

--- src/adapters/aave-v3/tests/testCases.ts
+++ src/adapters/aave-v3/tests/testCases.ts
@@ -9,6 +9,7 @@ export const testCases: TestCase[] = [
     input: {
       userAddress: '0x9957d6f3d37382c59db20e3cabe3e4540b52bba2',
     },
+    blockNumber: 140029880,
   },
   {
     chainId: Chain.Arbitrum,
@@ -17,17 +18,21 @@ export const testCases: TestCase[] = [
       userAddress: '0x9957d6f3d37382c59db20e3cabe3e4540b52bba2',
       timePeriod: TimePeriod.oneDay,
     },
+    blockNumber: 140029883,
   },
   {
     chainId: Chain.Arbitrum,
     method: 'tvl',
+    blockNumber: 140029886,
   },
   {
     chainId: Chain.Arbitrum,
     method: 'apr',
+    blockNumber: 140029892,
   },
   {
     chainId: Chain.Arbitrum,
     method: 'apy',
+    blockNumber: 140029896,
   },
 ]
