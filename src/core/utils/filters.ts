072d61646fddd2b2dae595a3b30a7a2a40244a2f
Bernardo Garces Chapero <bernardo.chapero@consensys.net>
2023-10-18 15:36:00 +0100

    restore changes

--- src/core/utils/filters.ts
+++ src/core/utils/filters.ts
@@ -2,7 +2,9 @@ export const filterMapSync = <Input, Return>(
   array: Input[],
   callback: (value: Input, index: number, array: Input[]) => Return | undefined,
 ): Return[] => {
-  return array.map(callback).filter(Boolean)
+  return array
+    .map(callback)
+    .filter((result) => result !== undefined) as Return[]
 }
 
 export const filterMapAsync = async <Input, Return>(
@@ -14,5 +16,5 @@ export const filterMapAsync = async <Input, Return>(
   ) => Promise<Return | undefined>,
 ): Promise<Return[]> => {
   const results = await Promise.all(array.map(callback))
-  return results.filter(Boolean)
+  return results.filter((result) => result !== undefined) as Return[]
 }
