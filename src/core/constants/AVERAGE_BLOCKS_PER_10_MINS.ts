8b2fab20b54a17f2089b68607074e204aa41dc1a
Bernardo Garces Chapero <bernardo.chapero@consensys.net>
2023-10-19 11:18:21 +0100

    fix blocks

--- src/core/constants/AVERAGE_BLOCKS_PER_10_MINS.ts
+++ src/core/constants/AVERAGE_BLOCKS_PER_10_MINS.ts
@@ -4,7 +4,9 @@ import { Chain } from './chains'
 export const AVERAGE_BLOCKS_PER_10_MINUTES: Record<Chain, number> =
   Object.entries(AVERAGE_BLOCKS_PER_DAY).reduce(
     (acc, [chain, blocksPerDay]) => {
-      acc[chain as unknown as Chain] = (blocksPerDay * 10) / (24 * 60)
+      acc[chain as unknown as Chain] = Math.floor(
+        (blocksPerDay * 10) / (24 * 60),
+      )
       return acc
     },
     {} as Record<Chain, number>,
