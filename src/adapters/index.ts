5e797b0a78f017f329a652f97f96fdfcfb8f1790
jp <jp@jps-MacBook-Pro.local>
2023-11-14 10:21:42 -0800

    feat: curve staking

--- src/adapters/index.ts
+++ src/adapters/index.ts
@@ -108,10 +108,6 @@ export const supportedProtocols: Record<
     [Chain.Ethereum]: [CurvePoolAdapter, CurveStakingAdapter],
   },
 
-  // [Protocol.Curve]: {
-  //   [Chain.Ethereum]: [CurveStakingAdapter],
-  // },
-
   [Protocol.Compound]: {
     [Chain.Ethereum]: [CompoundPoolAdapter],
   },
