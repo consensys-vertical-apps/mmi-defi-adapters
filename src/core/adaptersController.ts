fed004652c213c344e09a0a4b66bfe8e4da33d4a
Bernardo Garces Chapero <bernardo.chapero@consensys.net>
2023-11-21 16:53:20 +0000

    protocol token list

--- src/core/adaptersController.ts
+++ src/core/adaptersController.ts
@@ -9,6 +9,8 @@ export class AdaptersController {
   private adapters: Map<Chain, Map<Protocol, Map<string, IProtocolAdapter>>> =
     new Map()
 
+  private protocolTokens: Map<Chain, Map<string, IProtocolAdapter>> = new Map()
+
   constructor({
     providers,
     supportedProtocols,
@@ -65,6 +67,34 @@ export class AdaptersController {
         )
       },
     )
+  }
+
+  async fetchTokenAdapter(
+    chainId: Chain,
+    tokenAddress: string,
+  ): Promise<IProtocolAdapter | undefined> {
+    // TODO Memoize this
+    for (const [chainId, chainAdapters] of this.adapters) {
+      this.protocolTokens.set(chainId, new Map())
+      const chainAdaptersMap = this.protocolTokens.get(chainId)!
+
+      for (const [_protocolId, protocolAdapters] of chainAdapters) {
+        for (const [_productId, adapter] of protocolAdapters) {
+          const protocolTokens = await adapter.getProtocolTokens()
+
+          for (const protocolToken of protocolTokens) {
+            if (chainAdaptersMap.has(protocolToken.address)) {
+              // TODO THIS WON'T WORK WITH CURRENT REWARDS SYSTEM
+              throw Error('Duplicated protocol token')
+            }
+
+            chainAdaptersMap.set(protocolToken.address, adapter)
+          }
+        }
+      }
+    }
+
+    return this.protocolTokens.get(chainId)?.get(tokenAddress)
   }
 
   fetchAdapter(
