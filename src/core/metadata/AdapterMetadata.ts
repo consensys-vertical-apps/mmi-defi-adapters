70c66474203465e5d51042803de3f30bf4b8a37d
Tom Rpl <tomireppelin@gmail.com>
2023-12-13 13:57:39 +0100

    feat(imports): linting + final fixes

--- src/core/metadata/AdapterMetadata.ts
+++ src/core/metadata/AdapterMetadata.ts
@@ -42,10 +42,10 @@ import GMXGlpArbitrumGlp from '../../adapters/gmx/products/glp/metadata/arbitrum
 import GMXGlpAvalancheGlp from '../../adapters/gmx/products/glp/metadata/avalanche.glp.json'
 import MorphoAaveV2OptimizerBorrowEthereumOptimizerBorrow from '../../adapters/morpho-aave-v2/products/optimizer-borrow/metadata/ethereum.optimizer-borrow.json'
 import MorphoAaveV2OptimizerSupplyEthereumOptimizerSupply from '../../adapters/morpho-aave-v2/products/optimizer-supply/metadata/ethereum.optimizer-supply.json'
-import MorphoCompoundV2OptimizerBorrowEthereumOptimizerBorrow from '../../adapters/morpho-compound-v2/products/optimizer-borrow/metadata/ethereum.optimizer-borrow.json'
-import MorphoCompoundV2OptimizerSupplyEthereumOptimizerSupply from '../../adapters/morpho-compound-v2/products/optimizer-supply/metadata/ethereum.optimizer-supply.json'
 import MorphoAaveV3ETHOptimizerBorrowEthereumOptimizerBorrow from '../../adapters/morpho-aave-v3-eth/products/optimizer-borrow/metadata/ethereum.optimizer-borrow.json'
 import MorphoAaveV3ETHOptimizerSupplyEthereumOptimizerSupply from '../../adapters/morpho-aave-v3-eth/products/optimizer-supply/metadata/ethereum.optimizer-supply.json'
+import MorphoCompoundV2OptimizerBorrowEthereumOptimizerBorrow from '../../adapters/morpho-compound-v2/products/optimizer-borrow/metadata/ethereum.optimizer-borrow.json'
+import MorphoCompoundV2OptimizerSupplyEthereumOptimizerSupply from '../../adapters/morpho-compound-v2/products/optimizer-supply/metadata/ethereum.optimizer-supply.json'
 import PricesUsdEthereumProtocolToken from '../../adapters/prices/products/usd/metadata/ethereum.protocol-token.json'
 import { Protocol } from '../../adapters/protocols'
 import StargatePoolArbitrumLpToken from '../../adapters/stargate/products/pool/metadata/arbitrum.lp-token.json'
@@ -488,7 +488,7 @@ export const MetadataFiles = new Map<string, Json>([
     }),
     PricesUsdEthereumProtocolToken,
   ],
-    [
+  [
     metadataKey({
       protocolId: Protocol.MorphoCompoundV2,
       productId: 'optimizer-borrow',
@@ -506,14 +506,14 @@ export const MetadataFiles = new Map<string, Json>([
     }),
     MorphoCompoundV2OptimizerSupplyEthereumOptimizerSupply,
   ],
-    [
+  [
     metadataKey({
       protocolId: Protocol.MorphoAaveV3ETHOptimizer,
       productId: 'optimizer-borrow',
       chainId: Chain.Ethereum,
       fileKey: 'optimizer-borrow',
     }),
-    MorphoAaveV3ETHOptimizerOptimizerBorrowEthereumOptimizerBorrow,
+    MorphoAaveV3ETHOptimizerBorrowEthereumOptimizerBorrow,
   ],
   [
     metadataKey({
@@ -522,7 +522,7 @@ export const MetadataFiles = new Map<string, Json>([
       chainId: Chain.Ethereum,
       fileKey: 'optimizer-supply',
     }),
-    MorphoAaveV3ETHOptimizerOptimizerSupplyEthereumOptimizerSupply,
+    MorphoAaveV3ETHOptimizerSupplyEthereumOptimizerSupply,
   ],
 ])
 
