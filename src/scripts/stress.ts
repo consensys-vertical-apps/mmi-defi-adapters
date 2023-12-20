b6b4c90e83660361232181092642a0d2ba1f79c1
Bernardo Garces Chapero <bernardo.chapero@consensys.net>
2023-12-20 22:49:12 +0000

    stress test

---
+++ src/scripts/stress.ts
@@ -0,0 +1,352 @@
+import { Command } from 'commander'
+import { ethers } from 'ethers'
+import { Protocol } from '../adapters/protocols'
+import { Chain } from '../core/constants/chains'
+import { TimePeriod } from '../core/constants/timePeriod'
+import { DefiProvider } from '../defiProvider'
+import {
+  AdapterResponse,
+  DefiPositionResponse,
+  DefiProfitsResponse,
+} from '../types/response'
+import { multiChainFilter, multiProtocolFilter } from './commandFilters'
+
+export function stressCommand(program: Command, defiProvider: DefiProvider) {
+  program
+    .command('stress')
+    .option(
+      '-i, --iterations <iterations>',
+      'number of times that the test will be executed',
+      '1',
+    )
+    .option('-a, --address <address>', 'address of the target account')
+    .option(
+      '-p, --protocols <protocols>',
+      'comma-separated protocols filter (e.g. stargate,aave-v2)',
+    )
+    .option(
+      '-c, --chains <chains>',
+      'comma-separated chains filter (e.g. ethereum,arbitrum,linea)',
+    )
+
+    .option(
+      '-s, --strategy <strategy>',
+      'strategy to use (e.g. sequential-naive, parallel-naive, optimized)',
+    )
+    .showHelpAfterError()
+    .action(async ({ iterations, address, protocols, chains, strategy }) => {
+      const addresses = [...Array(Number(iterations)).keys()].map(() => {
+        return address ?? ethers.Wallet.createRandom().address
+      })
+
+      const filterProtocolIds = multiProtocolFilter(protocols)
+      const filterChainIds = multiChainFilter(chains)
+
+      console.log(`Run stress test`)
+      console.log(`Iterations: ${addresses.length}`)
+      if (filterChainIds) {
+        console.log(`Chains: ${filterChainIds.join(',')}`)
+      }
+      if (filterProtocolIds) {
+        console.log(`Protocols: ${filterProtocolIds.join(',')}`)
+      }
+
+      const blockNumbers = await defiProvider.getStableBlockNumbers(
+        filterChainIds,
+      )
+
+      const start = Date.now()
+
+      switch (strategy) {
+        case 'sequential-naive':
+          await sequentialNaive({
+            defiProvider,
+            addresses,
+            filterChainIds,
+            filterProtocolIds,
+            blockNumbers,
+            globalStart: start,
+          })
+          break
+        case 'parallel-naive':
+          await parallelNaive({
+            defiProvider,
+            addresses,
+            filterChainIds,
+            filterProtocolIds,
+            blockNumbers,
+            globalStart: start,
+          })
+          break
+        case 'optimized':
+          await optimized({
+            defiProvider,
+            addresses,
+            filterChainIds,
+            filterProtocolIds,
+            blockNumbers,
+            globalStart: start,
+          })
+          break
+        default:
+          throw new Error(`Invalid strategy: ${strategy}`)
+      }
+
+      const end = Date.now()
+      console.log(`Total: ${end - start}ms`)
+    })
+}
+
+async function parallelNaive({
+  defiProvider,
+  addresses,
+  filterChainIds,
+  filterProtocolIds,
+  blockNumbers,
+  globalStart,
+}: {
+  defiProvider: DefiProvider
+  addresses: string[]
+  filterChainIds: Chain[] | undefined
+  filterProtocolIds: Protocol[] | undefined
+  blockNumbers: Partial<Record<Chain, number>>
+  globalStart: number
+}) {
+  const promises = addresses.flatMap((userAddress) => {
+    return [
+      profileCall({
+        defiProvider,
+        method: 'positions',
+        userAddress,
+        filterChainIds,
+        filterProtocolIds,
+        blockNumbers,
+        globalStart,
+      }),
+      profileCall({
+        defiProvider,
+        method: 'profits',
+        timePeriod: TimePeriod.oneDay,
+        userAddress,
+        filterChainIds,
+        filterProtocolIds,
+        blockNumbers,
+        globalStart,
+      }),
+      profileCall({
+        defiProvider,
+        method: 'profits',
+        timePeriod: TimePeriod.sevenDays,
+        userAddress,
+        filterChainIds,
+        filterProtocolIds,
+        blockNumbers,
+        globalStart,
+      }),
+      profileCall({
+        defiProvider,
+        method: 'profits',
+        timePeriod: TimePeriod.thirtyDays,
+        userAddress,
+        filterChainIds,
+        filterProtocolIds,
+        blockNumbers,
+        globalStart,
+      }),
+    ]
+  })
+
+  await Promise.all(promises)
+}
+
+async function sequentialNaive({
+  defiProvider,
+  addresses,
+  filterChainIds,
+  filterProtocolIds,
+  blockNumbers,
+  globalStart,
+}: {
+  defiProvider: DefiProvider
+  addresses: string[]
+  filterChainIds: Chain[] | undefined
+  filterProtocolIds: Protocol[] | undefined
+  blockNumbers: Partial<Record<Chain, number>>
+  globalStart: number
+}) {
+  for (const userAddress of addresses) {
+    await profileCall({
+      defiProvider,
+      method: 'positions',
+      userAddress,
+      filterChainIds,
+      filterProtocolIds,
+      blockNumbers,
+      globalStart,
+    })
+
+    await profileCall({
+      defiProvider,
+      method: 'profits',
+      timePeriod: TimePeriod.oneDay,
+      userAddress,
+      filterChainIds,
+      filterProtocolIds,
+      blockNumbers,
+      globalStart,
+    })
+
+    await profileCall({
+      defiProvider,
+      method: 'profits',
+      timePeriod: TimePeriod.sevenDays,
+      userAddress,
+      filterChainIds,
+      filterProtocolIds,
+      blockNumbers,
+      globalStart,
+    })
+
+    await profileCall({
+      defiProvider,
+      method: 'profits',
+      timePeriod: TimePeriod.thirtyDays,
+      userAddress,
+      filterChainIds,
+      filterProtocolIds,
+      blockNumbers,
+      globalStart,
+    })
+  }
+}
+
+async function optimized({
+  defiProvider,
+  addresses,
+  filterChainIds,
+  filterProtocolIds,
+  blockNumbers,
+  globalStart,
+}: {
+  defiProvider: DefiProvider
+  addresses: string[]
+  filterChainIds: Chain[] | undefined
+  filterProtocolIds: Protocol[] | undefined
+  blockNumbers: Partial<Record<Chain, number>>
+  globalStart: number
+}) {
+  const promises = addresses.map(async (userAddress) => {
+    const positionsResponses = await profileCall({
+      defiProvider,
+      method: 'positions',
+      userAddress,
+      filterChainIds,
+      filterProtocolIds,
+      blockNumbers,
+      globalStart,
+    })
+
+    const protocolTokenAddresses = positionsResponses.flatMap((positions) => {
+      if (!positions.success) {
+        return []
+      }
+
+      return positions.tokens.map((token) => token.address)
+    })
+
+    const profitPromises = [
+      profileCall({
+        defiProvider,
+        method: 'profits',
+        timePeriod: TimePeriod.oneDay,
+        userAddress,
+        filterChainIds,
+        filterProtocolIds,
+        blockNumbers,
+        globalStart,
+        protocolTokenAddresses,
+      }),
+    ]
+
+    await Promise.all(profitPromises)
+  })
+
+  await Promise.all(promises)
+}
+
+type ProfileCallInput = {
+  defiProvider: DefiProvider
+  userAddress: string
+  filterChainIds: Chain[] | undefined
+  filterProtocolIds: Protocol[] | undefined
+  blockNumbers: Partial<Record<Chain, number>>
+  globalStart: number
+  protocolTokenAddresses?: string[]
+} & ({ method: 'positions' } | { method: 'profits'; timePeriod: TimePeriod })
+
+async function profileCall(
+  input: ProfileCallInput & { method: 'positions' },
+): Promise<DefiPositionResponse[]>
+async function profileCall(
+  input: ProfileCallInput & { method: 'profits' },
+): Promise<DefiProfitsResponse[]>
+async function profileCall(
+  input: ProfileCallInput,
+): Promise<DefiPositionResponse[] | DefiProfitsResponse[]> {
+  const {
+    defiProvider,
+    method,
+    userAddress,
+    filterChainIds,
+    filterProtocolIds,
+    blockNumbers,
+    globalStart,
+    protocolTokenAddresses,
+  } = input
+
+  const logPrefix = `[${
+    method === 'profits' ? `profits:${input.timePeriod}` : 'positions'
+  } # ${userAddress}]`
+
+  const start = Date.now()
+  console.log(`${logPrefix} Start: ${start - globalStart}ms`)
+
+  const result = await (method === 'profits'
+    ? defiProvider.getProfits({
+        userAddress,
+        timePeriod: input.timePeriod,
+        filterChainIds,
+        filterProtocolIds,
+        toBlockNumbersOverride: blockNumbers,
+        filterProtocolTokens: protocolTokenAddresses,
+      })
+    : defiProvider.getPositions({
+        userAddress,
+        filterChainIds,
+        filterProtocolIds,
+        blockNumbers,
+        filterProtocolTokens: protocolTokenAddresses,
+      }))
+
+  const end = Date.now()
+
+  console.log(
+    `${logPrefix} Total: ${end - start}ms - Tokens: ${returnedValues(result)}}`,
+  )
+
+  return result
+}
+
+function returnedValues(
+  result: AdapterResponse<{
+    tokens: unknown[]
+  }>[],
+) {
+  return result.reduce((accumulator, response) => {
+    if (!response.success) {
+      return accumulator
+    }
+
+    return response.tokens.length + accumulator
+  }, 0)
+}
