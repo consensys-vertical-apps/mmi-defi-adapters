dcc37dc2f7cc708b20de748a53be74543a25c35a
jpsains <32621022+jpsains@users.noreply.github.com>
2024-01-29 01:35:27 -0800

    feat: add Rocket Pool rETH adapter (#153)
    * feat: add Rocket Pool rETH adapter
    
    ---------
    
    Co-authored-by: Nick Doherty <nick@nickdoherty.com>

--- src/adapters/index.ts
+++ src/adapters/index.ts
@@ -28,6 +28,7 @@ import { MendiFinanceBorrowAdapter } from './mendi-finance/products/borrow/mendi
 import { MendiFinanceSupplyAdapter } from './mendi-finance/products/supply/mendiFinanceSupplyAdapter'
 import { PricesUSDAdapter } from './prices/products/usd/pricesUSDAdapter'
 import { Protocol } from './protocols'
+import { RocketPoolRethAdapter } from './rocket-pool/products/reth/rocketPoolRethAdapter'
 import { StargatePoolAdapter } from './stargate/products/pool/stargatePoolAdapter'
 import { StargateVestingAdapter } from './stargate/products/vesting/stargateVestingAdapter'
 import { SwellSwEthAdapter } from './swell/products/sw-eth/swellSwEthAdapter'
@@ -170,10 +171,16 @@ export const supportedProtocols: Record<
   [Protocol.SyncSwap]: {
     [Chain.Linea]: [SyncswapPoolAdapter],
   },
+
   [Protocol.MendiFinance]: {
     [Chain.Linea]: [MendiFinanceSupplyAdapter, MendiFinanceBorrowAdapter],
   },
+
   [Protocol.CarbonDeFi]: {
     [Chain.Ethereum]: [CarbonDeFiStrategiesAdapter],
+  },
+
+  [Protocol.RocketPool]: {
+    [Chain.Ethereum]: [RocketPoolRethAdapter],
   },
 }
