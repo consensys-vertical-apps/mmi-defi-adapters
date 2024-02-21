fe899fa0ef6239e9c3a79bb76b869f8aa8023257
Bernardo Garces Chapero <bernardo.chapero@consensys.net>
2024-02-21 12:46:13 +0000

    deposits and withdrawals

--- src/adapters/uniswap-v2/tests/testCases.ts
+++ src/adapters/uniswap-v2/tests/testCases.ts
@@ -1,19 +1,43 @@
+import { Chain } from '../../../core/constants/chains'
+import { TimePeriod } from '../../../core/constants/timePeriod'
 import type { TestCase } from '../../../types/testCase'
 
 export const testCases: TestCase[] = [
-  // {
-  //   chainId: Chain.Ethereum,
-  //   method: 'positions',
-  //   input: {
-  //     userAddress: '0x6b8Be925ED8277fE4D27820aE4677e76Ebf4c255',
-  //   },
-  // },
-  // {
-  //   chainId: Chain.Ethereum,
-  //   method: 'profits',
-  //   input: {
-  //     userAddress: '0xCEadFdCCd0E8E370D985c49Ed3117b2572243A4a',
-  //     timePeriod: TimePeriod.oneDay,
-  //   },
-  // },
+  {
+    chainId: Chain.Ethereum,
+    method: 'positions',
+    input: {
+      userAddress: '0xa3e8c7e7402565d4476661f37bd033bb8d960e49',
+    },
+  },
+  {
+    chainId: Chain.Ethereum,
+    method: 'profits',
+    input: {
+      userAddress: '0xa3e8c7e7402565d4476661f37bd033bb8d960e49',
+      timePeriod: TimePeriod.oneDay,
+    },
+  },
+  {
+    chainId: Chain.Ethereum,
+    method: 'deposits',
+    input: {
+      userAddress: '0xBA54C070B1f44DFb5fe161e5dC3062E2b965166D',
+      fromBlock: 19234490,
+      toBlock: 19234499,
+      productId: 'pool',
+      protocolTokenAddress: '0x0d4a11d5EEaaC28EC3F61d100daF4d40471f1852'
+    },
+  },
+  {
+    chainId: Chain.Ethereum,
+    method: 'withdrawals',
+    input: {
+      userAddress: '0x24D1da1D4b85F11EA7864fa1caac7A68a2A4e3Cc',
+      fromBlock: 19229200,
+      toBlock: 19229220,
+      productId: 'pool',
+      protocolTokenAddress: '0x0d4a11d5EEaaC28EC3F61d100daF4d40471f1852'
+    },
+  },
 ]
