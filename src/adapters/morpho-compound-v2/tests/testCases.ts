4554406050376594dacbe8ad87cc7f8ddc8ca6cc
JP <jp.sainsbury@consensys.net>
2024-02-22 13:47:47 +0000

    feat: morpho

--- src/adapters/morpho-compound-v2/tests/testCases.ts
+++ src/adapters/morpho-compound-v2/tests/testCases.ts
@@ -3,39 +3,39 @@ import { TimePeriod } from '../../../core/constants/timePeriod'
 import type { TestCase } from '../../../types/testCase'
 
 export const testCases: TestCase[] = [
-  // {
-  //   chainId: Chain.Ethereum,
-  //   method: 'positions',
-  //   input: {
-  //     userAddress: '0xc3fd2bcb524af31963b3e3bb670f28ba14718244',
-  //   },
-  //   blockNumber: 18776256,
-  // },
-  // {
-  //   chainId: Chain.Ethereum,
-  //   method: 'profits',
-  //   input: {
-  //     userAddress: '0xc3fd2bcb524af31963b3e3bb670f28ba14718244',
-  //     timePeriod: TimePeriod.oneDay,
-  //     includeRawValues: true,
-  //   },
-  //   blockNumber: 18802919 + 1,
-  // },
-  // {
-  //   chainId: Chain.Ethereum,
-  //   method: 'tvl',
-  //   blockNumber: 18776256,
-  // },
-  // {
-  //   chainId: Chain.Ethereum,
-  //   method: 'apr',
-  //   blockNumber: 18776256,
-  // },
-  // {
-  //   chainId: Chain.Ethereum,
-  //   method: 'apy',
-  //   blockNumber: 18776256,
-  // },
+  {
+    chainId: Chain.Ethereum,
+    method: 'positions',
+    input: {
+      userAddress: '0xc3fd2bcb524af31963b3e3bb670f28ba14718244',
+    },
+    blockNumber: 18776256,
+  },
+  {
+    chainId: Chain.Ethereum,
+    method: 'profits',
+    input: {
+      userAddress: '0xc3fd2bcb524af31963b3e3bb670f28ba14718244',
+      timePeriod: TimePeriod.oneDay,
+      includeRawValues: true,
+    },
+    blockNumber: 18802919 + 1,
+  },
+  {
+    chainId: Chain.Ethereum,
+    method: 'tvl',
+    blockNumber: 18776256,
+  },
+  {
+    chainId: Chain.Ethereum,
+    method: 'apr',
+    blockNumber: 18776256,
+  },
+  {
+    chainId: Chain.Ethereum,
+    method: 'apy',
+    blockNumber: 18776256,
+  },
   {
     chainId: Chain.Ethereum,
     method: 'withdrawals',
