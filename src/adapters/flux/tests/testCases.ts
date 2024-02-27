d5fca2046c68ce1684bae51ed91684fa80c38dd0
Daniel Salvadori <danaugrs@gmail.com>
2024-02-27 18:23:06 -0300

    Add test cases

--- src/adapters/flux/tests/testCases.ts
+++ src/adapters/flux/tests/testCases.ts
@@ -9,6 +9,7 @@ export const testCases: TestCase[] = [
     input: {
       userAddress: '0xd73B2cA04C6392df262dd756d3E4e27ad32308A6',
     },
+    blockNumber: 19321516,
   },
   {
     chainId: Chain.Ethereum,
@@ -16,6 +17,66 @@ export const testCases: TestCase[] = [
     input: {
       userAddress: '0xd73B2cA04C6392df262dd756d3E4e27ad32308A6',
       timePeriod: TimePeriod.oneDay,
+    },
+    blockNumber: 19321516,
+  },
+  {
+    chainId: Chain.Ethereum,
+    method: 'tvl',
+    blockNumber: 19321516,
+  },
+  {
+    chainId: Chain.Ethereum,
+    method: 'apr',
+    blockNumber: 19321516,
+  },
+  {
+    chainId: Chain.Ethereum,
+    method: 'apy',
+    blockNumber: 19321516,
+  },
+  {
+    chainId: Chain.Ethereum,
+    method: 'withdrawals',
+    input: {
+      userAddress: '0xd73B2cA04C6392df262dd756d3E4e27ad32308A6',
+      fromBlock: 14535107,
+      toBlock: 19321516,
+      protocolTokenAddress: '0x465a5a630482f3abD6d3b84B39B29b07214d19e5',
+      productId: 'pool',
+    },
+  },
+  {
+    chainId: Chain.Ethereum,
+    method: 'deposits',
+    input: {
+      userAddress: '0xd73B2cA04C6392df262dd756d3E4e27ad32308A6',
+      fromBlock: 14535107,
+      toBlock: 19321516,
+      protocolTokenAddress: '0x465a5a630482f3abD6d3b84B39B29b07214d19e5',
+      productId: 'pool',
+    },
+  },
+  {
+    chainId: Chain.Ethereum,
+    method: 'repays',
+    input: {
+      userAddress: '0xd73B2cA04C6392df262dd756d3E4e27ad32308A6',
+      fromBlock: 14535107,
+      toBlock: 19321516,
+      protocolTokenAddress: '0x465a5a630482f3abD6d3b84B39B29b07214d19e5',
+      productId: 'pool',
+    },
+  },
+  {
+    chainId: Chain.Ethereum,
+    method: 'borrows',
+    input: {
+      userAddress: '0xd73B2cA04C6392df262dd756d3E4e27ad32308A6',
+      fromBlock: 14535107,
+      toBlock: 19321516,
+      protocolTokenAddress: '0x465a5a630482f3abD6d3b84B39B29b07214d19e5',
+      productId: 'pool',
     },
   },
 ]
