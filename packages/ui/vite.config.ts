f9f5dfe0c0f6f1fea285f06967623107af71f0c2
Bernardo Garces Chapero <bernardo.chapero@consensys.net>
2024-04-23 15:11:47 +0100

    fixes

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv'
 
 dotenv.config({
-  path: path.resolve('../lib/.env'),
+  path: path.resolve('../../.env'),
 })
 
 const envs = [
@@ -20,9 +20,15 @@ const envs = [
   'DEFI_ADAPTERS_PROVIDER_ARBITRUM',
   'DEFI_ADAPTERS_PROVIDER_AVALANCHE',
   'DEFI_ADAPTERS_PROVIDER_LINEA',
+  'DEFI_ADAPTERS_ENABLE_USD_PRICES_FOR_POSITIONS',
+  'DEFI_ADAPTERS_RPC_CALL_TIMEOUT_IN_MS',
+  'DEFI_ADAPTERS_RPC_CALL_RETRIES',
+  'DEFI_ADAPTERS_RPC_GETLOGS_TIMEOUT_IN_MS',
+  'DEFI_ADAPTERS_RPC_GETLOGS_RETRIES',
+  'DEFI_ADAPTERS_USE_FAILOVER',
 ]
 
-const xxx = envs.reduce(
+const projectEnvs = envs.reduce(
   (acc, env) => {
     acc[`process.env.${env}`] = JSON.stringify(process.env[env])
     return acc
@@ -30,12 +36,10 @@ const xxx = envs.reduce(
   {} as Record<string, string>,
 )
 
-console.log('AAAAAAAAAAAAA', xxx)
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: xxx,
+  define: projectEnvs,
   optimizeDeps: {
     include: ['@metamask-institutional/defi-adapters'],
   },
