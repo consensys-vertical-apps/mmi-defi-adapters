2e6d880eebb16615355b6718cadb1fe51500bcf3
Bernardo Garces Chapero <bernardo.chapero@consensys.net>
2024-04-30 15:22:41 +0100

    eslint change

---
+++ packages/adapters-library/.eslintrc.js
@@ -0,0 +1,57 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'import', 'prettier', 'unused-imports'],
+  ignorePatterns: [
+    'packages/adapters-library/src/adapters/blank/blankTemplate/blankAdapterForCli/blankAdapter.ts',
+    'rebuild-and-restart.js',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'prettier',
+  ],
+  env: {
+    node: true,
+    jest: true,
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'arrow-body-style': 'off', // Disabled as per eslint-plugin-prettier recommendation
+    'prefer-arrow-callback': 'off', // Disabled as per eslint-plugin-prettier recommendation
+
+    // Disable the original rule
+    '@typescript-eslint/no-unused-vars': 'off',
+
+    // Add the auto-fixable rules from 'eslint-plugin-unused-imports'
+    'unused-imports/no-unused-imports-ts': 'error',
+    'unused-imports/no-unused-vars-ts': [
+      'error',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+
+    'import/order': [
+      'error',
+      {
+        alphabetize: { order: 'asc', caseInsensitive: true },
+        'newlines-between': 'never',
+      },
+    ],
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts'],
+    },
+    'import/resolver': {
+      typescript: true,
+      node: true,
+    },
+  },
+}
