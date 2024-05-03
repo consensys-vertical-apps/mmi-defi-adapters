c0ad326134bc4edb4ac45bb64ebbd0284e6a6f12
Bernardo Garces Chapero <bernardo.chapero@consensys.net>
2024-05-03 09:54:05 +0100

    merge tests for lido

--- packages/adapters-library/src/adapters/lido/tests/testCases.ts
+++ packages/adapters-library/src/adapters/lido/tests/testCases.ts
@@ -70,17 +70,12 @@ export const testCases: TestCase[] = [
     blockNumber: 19661884,
   },
   {
-    key: 'st-eth',
     chainId: Chain.Ethereum,
     method: 'tvl',
-    filterProtocolTokens: ['0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84'],
-    blockNumber: 19661884,
-  },
-  {
-    key: 'wst-eth',
-    chainId: Chain.Ethereum,
-    method: 'tvl',
-    filterProtocolTokens: ['0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0'],
+    filterProtocolTokens: [
+      '0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84',
+      '0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0',
+    ],
     blockNumber: 19661884,
   },
   {
