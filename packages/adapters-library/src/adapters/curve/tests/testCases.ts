8e6b698c6de6b9fe2891d8e50364b53cd948b8c2
Bernardo Garces Chapero <bernardo.chapero@consensys.net>
2024-05-07 15:30:32 +0100

    replace more arbitrum tests

--- packages/adapters-library/src/adapters/curve/tests/testCases.ts
+++ packages/adapters-library/src/adapters/curve/tests/testCases.ts
@@ -56,33 +56,6 @@ export const testCases: TestCase[] = [
     blockNumber: 19316971,
   },
   {
-    chainId: Chain.Arbitrum,
-    method: 'positions',
-    blockNumber: 186041228,
-    input: {
-      userAddress: '0x52f541764e6e90eebc5c21ff570de0e2d63766b6',
-
-      filterProtocolTokens: [
-        '0xCE5F24B7A95e9cBa7df4B54E911B4A3Dc8CDAf6f',
-        '0x95285Ea6fF14F80A2fD3989a6bAb993Bd6b5fA13',
-        '0xB08FEf57bFcc5f7bF0EF69C0c090849d497C8F8A',
-        '0x030786336Bc7833D4325404A25FE451e4fde9807',
-        '0xd2cE52833a2a70Cf6255c6374Bc44525a0FB93D8',
-      ],
-    },
-  },
-  {
-    chainId: Chain.Arbitrum,
-    method: 'profits',
-    blockNumber: 186041228,
-    input: {
-      timePeriod: TimePeriod.oneDay,
-      userAddress: '0xe6617823d8008dfccba01187c24d17ad35c432db',
-      includeRawValues: true,
-      filterProtocolTokens: ['0xCE5F24B7A95e9cBa7df4B54E911B4A3Dc8CDAf6f'],
-    },
-  },
-  {
     chainId: Chain.Ethereum,
     key: 'position2',
     method: 'positions',
