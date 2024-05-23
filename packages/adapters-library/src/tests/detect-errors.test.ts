033bb34e236b578c95a719880b035f1a58764adc
Bernardo Garces Chapero <bernardo.chapero@consensys.net>
2024-05-23 16:13:55 +0100

    detect new errors

---
+++ packages/adapters-library/src/tests/detect-errors.test.ts
@@ -0,0 +1,20 @@
+import { DefiProvider } from '../defiProvider'
+
+describe('detect errors', () => {
+  it('does not return any adapter errors', async () => {
+    const defiProvider = new DefiProvider()
+    const positionsResponse = await defiProvider.getPositions({
+      userAddress: '0xaa62cf7caaf0c7e50deaa9d5d0b907472f00b258',
+    })
+
+    const positionsWithErrors = positionsResponse.filter(
+      (position) =>
+        !position.success &&
+        !['NotApplicableError', 'NotImplementedError'].includes(
+          position.error.details?.name,
+        ),
+    )
+
+    expect(positionsWithErrors).toEqual([])
+  }, 60000)
+})
