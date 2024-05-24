f5c136208003afab6a7e63767a612883a1313313
Bernardo Garces Chapero <bernardo.chapero@consensys.net>
2024-05-24 09:44:17 +0100

    add profits test

--- packages/adapters-library/src/tests/detect-errors.test.ts
+++ packages/adapters-library/src/tests/detect-errors.test.ts
@@ -1,20 +1,34 @@
 import { DefiProvider } from '../defiProvider'
+import { DefiPositionResponse, DefiProfitsResponse } from '../types/response'
 
 describe('detect errors', () => {
-  it('does not return any adapter errors', async () => {
+  it('does not return any adapter error with positions', async () => {
     const defiProvider = new DefiProvider()
-    const positionsResponse = await defiProvider.getPositions({
+    const response = await defiProvider.getPositions({
       userAddress: '0xaa62cf7caaf0c7e50deaa9d5d0b907472f00b258',
     })
 
-    const positionsWithErrors = positionsResponse.filter(
-      (position) =>
-        !position.success &&
-        !['NotApplicableError', 'NotImplementedError'].includes(
-          position.error.details?.name,
-        ),
-    )
+    expect(filterErrors(response)).toEqual([])
+  }, 60000)
 
-    expect(positionsWithErrors).toEqual([])
+  it('does not return any adapter error with profits', async () => {
+    const defiProvider = new DefiProvider()
+    const response = await defiProvider.getProfits({
+      userAddress: '0xaa62cf7caaf0c7e50deaa9d5d0b907472f00b258',
+    })
+
+    expect(filterErrors(response)).toEqual([])
   }, 60000)
 })
+
+function filterErrors(
+  response: DefiPositionResponse[] | DefiProfitsResponse[],
+) {
+  return response.filter(
+    (responseEntry) =>
+      !responseEntry.success &&
+      !['NotApplicableError', 'NotImplementedError'].includes(
+        responseEntry.error.details?.name,
+      ),
+  )
+}
