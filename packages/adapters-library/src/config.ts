ffbc795ebf79ffe1a3e9241ee40db2d9d56e4ebe
Bernardo Garces Chapero <bernardo.chapero@consensys.net>
2024-06-03 12:56:18 +0100

    env parsing for numbers

--- packages/adapters-library/src/config.ts
+++ packages/adapters-library/src/config.ts
@@ -75,19 +75,31 @@ const ConfigSchema = z
     rpcCallTimeoutInMs: z
       .number()
       .default(
-        Number(process.env.DEFI_ADAPTERS_RPC_CALL_TIMEOUT_IN_MS) || 20000,
+        process.env.DEFI_ADAPTERS_RPC_CALL_TIMEOUT_IN_MS
+          ? Number(process.env.DEFI_ADAPTERS_RPC_CALL_TIMEOUT_IN_MS)
+          : 10000,
       ),
     rpcCallRetries: z
       .number()
-      .default(Number(process.env.DEFI_ADAPTERS_RPC_CALL_RETRIES) || 1),
+      .default(
+        process.env.DEFI_ADAPTERS_RPC_CALL_RETRIES
+          ? Number(process.env.DEFI_ADAPTERS_RPC_CALL_RETRIES)
+          : 0,
+      ),
     rpcGetLogsTimeoutInMs: z
       .number()
       .default(
-        Number(process.env.DEFI_ADAPTERS_RPC_GETLOGS_TIMEOUT_IN_MS) || 10000,
+        process.env.DEFI_ADAPTERS_RPC_GETLOGS_TIMEOUT_IN_MS
+          ? Number(process.env.DEFI_ADAPTERS_RPC_GETLOGS_TIMEOUT_IN_MS)
+          : 10000,
       ),
     rpcGetLogsRetries: z
       .number()
-      .default(Number(process.env.DEFI_ADAPTERS_RPC_GETLOGS_RETRIES) || 1),
+      .default(
+        process.env.DEFI_ADAPTERS_RPC_GETLOGS_RETRIES
+          ? Number(process.env.DEFI_ADAPTERS_RPC_GETLOGS_RETRIES)
+          : 0,
+      ),
     enableFailover: z
       .boolean()
       .default(process.env.DEFI_ADAPTERS_USE_FAILOVER !== 'false'),
