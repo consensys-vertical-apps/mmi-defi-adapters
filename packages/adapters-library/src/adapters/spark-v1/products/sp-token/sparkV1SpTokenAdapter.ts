d137d13fb472e03cf7eece038d98e0312a4883a9
Hannah Redler <hannahjredler@gmail.com>
2024-06-17 18:04:51 +0200

    Cleaning up unused imports and code

--- packages/adapters-library/src/adapters/spark-v1/products/sp-token/sparkV1SpTokenAdapter.ts
+++ packages/adapters-library/src/adapters/spark-v1/products/sp-token/sparkV1SpTokenAdapter.ts
@@ -1,5 +1,4 @@
 import { z } from 'zod'
-import { AdaptersController } from '../../../../core/adaptersController'
 import { Chain } from '../../../../core/constants/chains'
 import { GetTransactionParams } from '../../../supportedProtocols'
 import { PoolContract__factory } from '../../contracts'
@@ -7,13 +6,7 @@ import {
   CacheToFile,
   IMetadataBuilder,
 } from '../../../../core/decorators/cacheToFile'
-import { NotImplementedError } from '../../../../core/errors/errors'
-import { CustomJsonRpcProvider } from '../../../../core/provider/CustomJsonRpcProvider'
 import { logger } from '../../../../core/utils/logger'
-import { Helpers } from '../../../../scripts/helpers'
-import { Replacements } from '../../../../scripts/replacements'
-import { RewardsAdapter } from '../../../../scripts/rewardAdapter'
-import { IProtocolAdapter } from '../../../../types/IProtocolAdapter'
 import { getTokenMetadata } from '../../../../core/utils/getTokenMetadata'
 import {
   WriteActionInputSchemas,
@@ -22,19 +15,8 @@ import {
 
 import {
   AssetType,
-  GetEventsInput,
-  GetPositionsInput,
-  GetRewardPositionsInput,
-  GetTotalValueLockedInput,
-  MovementsByBlock,
   PositionType,
-  ProtocolAdapterParams,
   ProtocolDetails,
-  ProtocolPosition,
-  ProtocolTokenTvl,
-  UnderlyingReward,
-  UnwrapExchangeRate,
-  UnwrapInput,
   TokenBalance,
   TokenType,
   Underlying,
@@ -47,8 +29,8 @@ import {
   ProtocolDataProvider__factory,
 } from '../../contracts'
 import { SimplePoolAdapter } from '../../../../core/adapters/SimplePoolAdapter'
-const PRICE_PEGGED_TO_ONE = 1
 
+const PRICE_PEGGED_TO_ONE = 1
 const sparkEthereumProviderAddress = "0xFc21d6d146E6086B8359705C8b28512a983db0cb"
 
 type SparkMetadata = Record<
@@ -64,27 +46,6 @@ export class SparkV1SpTokenAdapter
   implements IMetadataBuilder
 {
   productId = 'sp-token'
-  // protocolId: Protocol
-
-  // helpers: Helpers
-
-  // private provider: CustomJsonRpcProvider
-
-  // adaptersController: AdaptersController
-
-  // constructor({
-  //   provider,
-  //   chainId,
-  //   protocolId,
-  //   adaptersController,
-  //   helpers,
-  // }: ProtocolAdapterParams) {
-  //   this.provider = provider
-  //   this.chainId = chainId
-  //   this.protocolId = protocolId
-  //   this.adaptersController = adaptersController
-  //   this.helpers = helpers
-  // }
 
   getProtocolDetails(): ProtocolDetails {
     return {
@@ -230,67 +191,6 @@ export class SparkV1SpTokenAdapter
         },
       ]
     }
-  // async getPositions(input: GetPositionsInput): Promise<ProtocolPosition[]> {
-  //   return this.helpers.getBalanceOfTokens({
-  //     ...input,
-  //     protocolTokens: await this.getProtocolTokens(),
-  //   })
-  // }
-
-  // async getWithdrawals({
-  //   protocolTokenAddress,
-  //   fromBlock,
-  //   toBlock,
-  //   userAddress,
-  // }: GetEventsInput): Promise<MovementsByBlock[]> {
-  //   return this.helpers.withdrawals({
-  //     protocolToken: await this.getProtocolToken(protocolTokenAddress),
-  //     filter: { fromBlock, toBlock, userAddress },
-  //   })
-  // }
-
-  // async getDeposits({
-  //   protocolTokenAddress,
-  //   fromBlock,
-  //   toBlock,
-  //   userAddress,
-  // }: GetEventsInput): Promise<MovementsByBlock[]> {
-  //   return this.helpers.deposits({
-  //     protocolToken: await this.getProtocolToken(protocolTokenAddress),
-  //     filter: { fromBlock, toBlock, userAddress },
-  //   })
-  // }
-
-  // async getTotalValueLocked({
-  //   protocolTokenAddresses,
-  //   blockNumber,
-  // }: GetTotalValueLockedInput): Promise<ProtocolTokenTvl[]> {
-  //   const protocolTokens = await this.getProtocolTokens()
-
-  //   return await this.helpers.tvl({
-  //     protocolTokens,
-  //     filterProtocolTokenAddresses: protocolTokenAddresses,
-  //     blockNumber,
-  //   })
-  // }
-
-  // async unwrap({
-  //   protocolTokenAddress,
-  //   tokenId,
-  //   blockNumber,
-  // }: UnwrapInput): Promise<UnwrapExchangeRate> {
-  //   return this.helpers.unwrapOneToOne({
-  //     protocolToken: await this.getProtocolToken(protocolTokenAddress),
-  //     underlyingTokens: await this.getUnderlyingTokens(protocolTokenAddress),
-  //   })
-  // }
-
-  // private async getProtocolToken(protocolTokenAddress: string) {
-  //   return (await this.fetchPoolMetadata(protocolTokenAddress)).protocolToken
-  // }
-  // private async getUnderlyingTokens(protocolTokenAddress: string) {
-  //   return (await this.fetchPoolMetadata(protocolTokenAddress)).underlyingTokens
-  // }
 
   private async fetchPoolMetadata(protocolTokenAddress: string) {
     const poolMetadata = (await this.buildMetadata())[protocolTokenAddress]
