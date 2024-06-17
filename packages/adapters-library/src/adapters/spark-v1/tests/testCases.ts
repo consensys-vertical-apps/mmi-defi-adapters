48977f9657879b75cae31398dbe962b37d2c30d4
Hannah Redler <hannahjredler@gmail.com>
2024-06-17 17:11:46 +0200

    Add read test cases

--- packages/adapters-library/src/adapters/spark-v1/tests/testCases.ts
+++ packages/adapters-library/src/adapters/spark-v1/tests/testCases.ts
@@ -4,21 +4,43 @@ import type { TestCase } from '../../../types/testCase'
 import { WriteActions } from '../../../types/writeActions'
 
 export const testCases: TestCase[] = [
-  // {
-  //   chainId: Chain.Ethereum,
-  //   method: 'positions',
-  //   input: {
-  //     userAddress: '0x6b8Be925ED8277fE4D27820aE4677e76Ebf4c255',
-  //   },
-  // },
-  // {
-  //   chainId: Chain.Ethereum,
-  //   method: 'profits',
-  //   input: {
-  //     userAddress: '0xCEadFdCCd0E8E370D985c49Ed3117b2572243A4a',
-  //     timePeriod: TimePeriod.oneDay,
-  //   },
-  // },
+  {
+    chainId: Chain.Ethereum,
+    method: 'positions',
+
+    input: {
+      userAddress: '0xf8dE75c7B95edB6f1E639751318f117663021Cf0',
+
+      filterProtocolTokens: ['0x59cD1C87501baa753d0B5B5Ab5D8416A45cD71DB'],
+    },
+
+    blockNumber: 20112246,
+  },
+  {
+    chainId: Chain.Ethereum,
+    method: 'profits',
+
+    input: {
+      userAddress: '0xf8dE75c7B95edB6f1E639751318f117663021Cf0',
+      timePeriod: TimePeriod.oneDay,
+
+      filterProtocolTokens: ['0x59cD1C87501baa753d0B5B5Ab5D8416A45cD71DB'],
+    },
+
+    blockNumber: 20112246,
+  },
+  {
+    chainId: Chain.Ethereum,
+    method: 'tvl',
+
+    filterProtocolTokens: [
+      '0x59cD1C87501baa753d0B5B5Ab5D8416A45cD71DB',
+      '0x12B54025C112Aa61fAce2CDB7118740875A566E9',
+    ],
+
+    blockNumber: 20112246,
+  },
+
   // {
   //   method: 'tx-params',
   //   key: 'supply',
@@ -27,8 +49,55 @@ export const testCases: TestCase[] = [
   //     productId: 'sp-token',
   //     action: WriteActions.Deposit,
   //     inputs: {
-  //       asset: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+  //       asset: '0x59cD1C87501baa753d0B5B5Ab5D8416A45cD71DB',
   //       amount: '10000000000000000000',
+  //       onBehalfOf: '0xf8dE75c7B95edB6f1E639751318f117663021Cf0',
+  //       referralCode: 0,
+  //     },
+  //   },
+  // },
+  // {
+  //   method: 'tx-params',
+  //   key: 'borrow',
+  //   chainId: Chain.Ethereum,
+  //   input: {
+  //     productId: 'sp-token',
+  //     action: WriteActions.Borrow,
+  //     inputs: {
+  //       asset: '0x6B175474E89094C44Da98b954EedeAC495271d0F',
+  //       amount: '10000000000000000000',
+  //       interestRateMode: 1,
+  //       referralCode: 0,
+  //       onBehalfOf: '0xf8dE75c7B95edB6f1E639751318f117663021Cf0',
+  //     },
+  //   },
+  // },
+  // {
+  //   method: 'tx-params',
+  //   key: 'withdraw',
+  //   chainId: Chain.Ethereum,
+  //   input: {
+  //     productId: 'sp-token',
+  //     action: WriteActions.Withdraw,
+  //     inputs: {
+  //       asset: '0x59cD1C87501baa753d0B5B5Ab5D8416A45cD71DB',
+  //       amount: '10000000000000000000',
+  //       to: '0xf8dE75c7B95edB6f1E639751318f117663021Cf0',
+  //     },
+  //   },
+  // },
+  // {
+  //   method: 'tx-params',
+  //   key: 'repay',
+  //   chainId: Chain.Ethereum,
+  //   input: {
+  //     productId: 'sp-token',
+  //     action: WriteActions.Repay,
+  //     inputs: {
+  //       asset: '0x6B175474E89094C44Da98b954EedeAC495271d0F',
+  //       amount: '10000000000000000000',
+  //       interestRateMode: 1,
+  //       onBehalfOf: '0xf8dE75c7B95edB6f1E639751318f117663021Cf0',
   //     },
   //   },
   // },
