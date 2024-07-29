1206f51c1282bb78147ce1a83ff7f20bf2c08d1e
Bernardo Garces Chapero <bernardo.chapero@consensys.net>
2024-07-29 16:33:16 +0100

    pancakeswap

--- packages/adapters-library/src/adapters/pancakeswap-v2/tests/testCases.ts
+++ packages/adapters-library/src/adapters/pancakeswap-v2/tests/testCases.ts
@@ -7,14 +7,10 @@ export const testCases: TestCase[] = [
     method: 'positions',
 
     input: {
-      userAddress: '0x53Dc97Eb9aFefBeBe3BF83aDD14fD812C69193FC',
-
-      filterProtocolTokens: [
-        '0x804678fa97d91B974ec2af3c843270886528a9E6',
-        '0x0eD7e52944161450477ee417DE9Cd3a859b14fD0',
-      ],
+      userAddress: '0xE8EEDa4230E37e5a136575151Cedb5f57Fa1cA99',
+      filterProtocolTokens: ['0x16b9a82891338f9bA80E2D6970FddA79D1eb0daE'],
     },
 
-    blockNumber: 36672150,
+    blockNumber: 40899757,
   },
 ]
