c1e31c74c884452d14a99cfc74c3db28954be8e7
Xavier Brochard <xavier.brochard@consensys.net>
2024-08-16 10:57:58 +0200

    feat: add support to SolvBTC.BBN and SolvBTC.ENA

--- packages/adapters-library/src/adapters/solv/products/solv-btc/config.ts
+++ packages/adapters-library/src/adapters/solv/products/solv-btc/config.ts
@@ -1,4 +1,5 @@
 import { getAddress } from 'ethers'
+import { mergeWith } from 'lodash'
 import { Chain } from '../../../../core/constants/chains'
 
 export interface TokenInfo {
@@ -6,21 +7,89 @@ export interface TokenInfo {
   underlyingToken: string
 }
 
-export const TokenAddresses: Partial<Record<Chain, TokenInfo>> = {
-  [Chain.Ethereum]: {
-    protocolToken: getAddress('0x7a56e1c57c7475ccf742a1832b028f0456652f97'),
-    underlyingToken: getAddress('0xC96dE26018A54D51c097160568752c4E3BD6C364'), // FBTC
-  },
-  [Chain.Bsc]: {
-    protocolToken: getAddress('0x4aae823a6a0b376de6a78e74ecc5b079d38cbcf7'),
-    underlyingToken: getAddress('0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c'), // BTCB
-  },
-  [Chain.Arbitrum]: {
-    protocolToken: getAddress('0x3647c54c4c2c65bc7a2d63c0da2809b399dbbdc0'),
-    underlyingToken: getAddress('0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f'), // WBTC
-  },
-  [Chain.Avalanche]: {
-    protocolToken: getAddress('0xbc78d84ba0c46dfe32cf2895a19939c86b81a777'),
-    underlyingToken: getAddress('0x152b9d0FdC40C096757F570A51E494bd4b943E50'), // BTC.b
-  },
+const SolvBtcTokenAddresses: Partial<Record<Chain, TokenInfo[]>> = {
+  [Chain.Ethereum]: [
+    {
+      protocolToken: getAddress('0x7a56e1c57c7475ccf742a1832b028f0456652f97'),
+      underlyingToken: getAddress('0xC96dE26018A54D51c097160568752c4E3BD6C364'), // FBTC
+    },
+  ],
+  [Chain.Bsc]: [
+    {
+      protocolToken: getAddress('0x4aae823a6a0b376de6a78e74ecc5b079d38cbcf7'),
+      underlyingToken: getAddress('0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c'), // BTCB
+    },
+  ],
+  [Chain.Arbitrum]: [
+    {
+      protocolToken: getAddress('0x3647c54c4c2c65bc7a2d63c0da2809b399dbbdc0'),
+      underlyingToken: getAddress('0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f'), // WBTC
+    },
+  ],
+  [Chain.Avalanche]: [
+    {
+      protocolToken: getAddress('0xbc78d84ba0c46dfe32cf2895a19939c86b81a777'),
+      underlyingToken: getAddress('0x152b9d0FdC40C096757F570A51E494bd4b943E50'), // BTC.b
+    },
+  ],
 }
+
+const SolvBtcBbnTokenAddresses: Partial<Record<Chain, TokenInfo[]>> = {
+  [Chain.Ethereum]: [
+    {
+      protocolToken: getAddress('0xd9d920aa40f578ab794426f5c90f6c731d159def'),
+      underlyingToken: SolvBtcTokenAddresses[Chain.Ethereum]![0]!.protocolToken,
+    },
+  ],
+  [Chain.Bsc]: [
+    {
+      protocolToken: getAddress('0x1346b618dc92810ec74163e4c27004c921d446a5'),
+      underlyingToken: SolvBtcTokenAddresses[Chain.Bsc]![0]!.protocolToken,
+    },
+  ],
+  [Chain.Arbitrum]: [
+    {
+      protocolToken: getAddress('0x346c574c56e1a4aaa8dc88cda8f7eb12b39947ab'),
+      underlyingToken: SolvBtcTokenAddresses[Chain.Arbitrum]![0]!.protocolToken,
+    },
+  ],
+}
+
+const SolvBtcEnaTokenAddresses: Partial<Record<Chain, TokenInfo[]>> = {
+  [Chain.Ethereum]: [
+    {
+      protocolToken: getAddress('0x325dc9ebcec31940c658acaca45f8293418d811e'),
+      underlyingToken: SolvBtcTokenAddresses[Chain.Ethereum]![0]!.protocolToken,
+    },
+  ],
+  [Chain.Bsc]: [
+    {
+      protocolToken: getAddress('0x53e63a31fd1077f949204b94f431bcab98f72bce'),
+      underlyingToken: SolvBtcTokenAddresses[Chain.Bsc]![0]!.protocolToken,
+    },
+  ],
+  [Chain.Arbitrum]: [
+    {
+      protocolToken: getAddress('0xafafd68afe3fe65d376eec9eab1802616cfaccb8'),
+      underlyingToken: SolvBtcTokenAddresses[Chain.Arbitrum]![0]!.protocolToken,
+    },
+  ],
+}
+
+/**
+ * Merge the above records in a single object strcutured like this:
+ *
+ * {
+ *   '1': [ {protocolToken: '...', underlyingToken: '...'}, {protocolToken: '...', underlyingToken: '...'}, ...]
+ *   '56': [ {protocolToken: '...', underlyingToken: '...'}, {protocolToken: '...', underlyingToken: '...'}, ...]
+ *   ...
+ * }
+ */
+export const TokenAddresses = mergeWith(
+  {},
+  SolvBtcTokenAddresses,
+  SolvBtcBbnTokenAddresses,
+  SolvBtcEnaTokenAddresses,
+  (objValue, srcValue) =>
+    Array.isArray(objValue) ? objValue.concat(srcValue) : undefined,
+)
--- packages/adapters-library/src/adapters/solv/tests/testCases.ts
+++ packages/adapters-library/src/adapters/solv/tests/testCases.ts
@@ -1,38 +1,34 @@
 import { Chain } from '../../../core/constants/chains'
 import type { TestCase } from '../../../types/testCase'
+import { TokenAddresses } from '../products/solv-btc/config'
+
+const User1 = '0x7BFEe91193d9Df2Ac0bFe90191D40F23c773C060' // Has some SolvBTC
+const User2 = '0xEBFAEEDE1D85E8E87BDe9326bc301830D55dfa8c' // Has some SolvBTC.BBN
+const User3 = '0x423e5E0ee2615E6bef4B181400553066dAE3b6fD' // Has some SolvBTC.ENA
+const User4 = '0xd87D6D2D766b15cDA45e3cACC8742104B5A921ea' // Has deposited + withdrew some SolvBTC over a few days
+
+const buildTestCase = (
+  chainId: Chain,
+  method: 'positions' | 'profits',
+  address: string,
+) => ({
+  chainId,
+  method,
+  input: {
+    userAddress: address,
+    filterProtocolTokens: TokenAddresses[chainId]!.map(
+      (tokenInfo) => tokenInfo.protocolToken,
+    ),
+  },
+})
 
 export const testCases: TestCase[] = [
-  {
-    chainId: Chain.Ethereum,
-    method: 'positions',
-
-    input: {
-      userAddress: '0x7BFEe91193d9Df2Ac0bFe90191D40F23c773C060',
-      filterProtocolTokens: ['0x7A56E1C57C7475CCf742a1832B028F0456652F97'],
-    },
-
-    blockNumber: 20520223,
-  },
-  {
-    chainId: Chain.Arbitrum,
-    method: 'positions',
-
-    input: {
-      userAddress: '0x7BFEe91193d9Df2Ac0bFe90191D40F23c773C060',
-      filterProtocolTokens: ['0x3647c54c4c2C65bC7a2D63c0Da2809B399DBBDC0'],
-    },
-
-    blockNumber: 242460102,
-  },
-  {
-    chainId: Chain.Arbitrum,
-    method: 'profits',
-
-    input: {
-      userAddress: '0xd87D6D2D766b15cDA45e3cACC8742104B5A921ea',
-      filterProtocolTokens: ['0x3647c54c4c2C65bC7a2D63c0Da2809B399DBBDC0'],
-    },
-
-    blockNumber: 242737820,
-  },
+  buildTestCase(Chain.Ethereum, 'positions', User1),
+  buildTestCase(Chain.Arbitrum, 'positions', User1),
+  buildTestCase(Chain.Ethereum, 'positions', User2),
+  buildTestCase(Chain.Bsc, 'positions', User2),
+  buildTestCase(Chain.Arbitrum, 'positions', User2),
+  buildTestCase(Chain.Bsc, 'positions', User3),
+  buildTestCase(Chain.Ethereum, 'profits', User4),
+  buildTestCase(Chain.Arbitrum, 'profits', User4),
 ]
