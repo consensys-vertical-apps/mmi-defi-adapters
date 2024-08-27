36f6d7f1f2c3cfd44c26a1394bf236703ea90dd4
Bernardo Garces Chapero <bernardo.chapero@consensys.net>
2024-08-27 15:59:28 +0100

    fix

--- packages/adapters-library/src/core/utils/unwrap.ts
+++ packages/adapters-library/src/core/utils/unwrap.ts
@@ -41,57 +41,59 @@ async function unwrapToken(
       token.address,
     )
 
-  // Try to fetch prices if there is no tokens and no adapter to resolve
-  if (!underlyingProtocolTokenAdapter) {
-    const tokenPriceRaw = await fetchPrice(adapter, token, blockNumber)
-    if (tokenPriceRaw) {
-      token.priceRaw = tokenPriceRaw
-    }
-  } else {
-    // Populate underlying tokens if there is an adapter for this token
-    const unwrapExchangeRates = await fetchUnwrapExchangeRates(
-      underlyingProtocolTokenAdapter,
-      token,
-      blockNumber,
-    )
-
-    if (unwrapExchangeRates?.tokens) {
-      if (!token.tokens) {
-        token.tokens = []
+  if (!token.tokens?.some((t) => t.type === TokenType.Underlying)) {
+    if (!underlyingProtocolTokenAdapter) {
+      // Try to fetch prices if there is no tokens and no adapter to resolve
+      const tokenPriceRaw = await fetchPrice(adapter, token, blockNumber)
+      if (tokenPriceRaw) {
+        token.priceRaw = tokenPriceRaw
       }
-
-      token.tokens.push(
-        ...filterMapSync(
-          unwrapExchangeRates.tokens,
-          (unwrappedTokenExchangeRate) => {
-            if (
-              token.tokens?.find(
-                (t) =>
-                  t.type !== TokenType.UnderlyingClaimable &&
-                  t.type !== TokenType.Reward &&
-                  t.address === unwrappedTokenExchangeRate.address,
-              )
-            ) {
-              return
-            }
-
-            const underlyingToken = {
-              address: unwrappedTokenExchangeRate.address,
-              name: unwrappedTokenExchangeRate.name,
-              symbol: unwrappedTokenExchangeRate.symbol,
-              decimals: unwrappedTokenExchangeRate.decimals,
-              type: UnderlyingTokenTypeMap[token.type],
-              [fieldToUpdate]:
-              // biome-ignore lint/suspicious/noExplicitAny: Too many possible options
-                (((token as any)[fieldToUpdate] as bigint) *
-                  unwrappedTokenExchangeRate.underlyingRateRaw) /
-                10n ** BigInt(token.decimals),
-            }
-
-            return underlyingToken
-          },
-        ),
+    } else {
+      // Populate underlying tokens if there is an adapter for this token
+      const unwrapExchangeRates = await fetchUnwrapExchangeRates(
+        underlyingProtocolTokenAdapter,
+        token,
+        blockNumber,
       )
+
+      if (unwrapExchangeRates?.tokens) {
+        if (!token.tokens) {
+          token.tokens = []
+        }
+
+        token.tokens.push(
+          ...filterMapSync(
+            unwrapExchangeRates.tokens,
+            (unwrappedTokenExchangeRate) => {
+              if (
+                token.tokens?.find(
+                  (t) =>
+                    t.type !== TokenType.UnderlyingClaimable &&
+                    t.type !== TokenType.Reward &&
+                    t.address === unwrappedTokenExchangeRate.address,
+                )
+              ) {
+                return
+              }
+
+              const underlyingToken = {
+                address: unwrappedTokenExchangeRate.address,
+                name: unwrappedTokenExchangeRate.name,
+                symbol: unwrappedTokenExchangeRate.symbol,
+                decimals: unwrappedTokenExchangeRate.decimals,
+                type: UnderlyingTokenTypeMap[token.type],
+                [fieldToUpdate]:
+                // biome-ignore lint/suspicious/noExplicitAny: Too many possible options
+                  (((token as any)[fieldToUpdate] as bigint) *
+                    unwrappedTokenExchangeRate.underlyingRateRaw) /
+                  10n ** BigInt(token.decimals),
+              }
+
+              return underlyingToken
+            },
+          ),
+        )
+      }
     }
   }
 
