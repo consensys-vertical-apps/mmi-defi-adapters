c277de769b283eaf9df0c9ec56f3f47f62b048d4
Bernardo Garces Chapero <bernardo.chapero@consensys.net>
2024-08-27 14:21:37 +0100

    lint

--- packages/adapters-library/src/core/utils/unwrap.ts
+++ packages/adapters-library/src/core/utils/unwrap.ts
@@ -83,7 +83,7 @@ async function unwrapToken(
           decimals: unwrappedTokenExchangeRate.decimals,
           type: UnderlyingTokenTypeMap[token.type],
           [fieldToUpdate]:
-          // biome-ignore lint/suspicious/noExplicitAny: Too many possible options
+            // biome-ignore lint/suspicious/noExplicitAny: Too many possible options
             (((token as any)[fieldToUpdate] as bigint) *
               unwrappedTokenExchangeRate.underlyingRateRaw) /
             10n ** BigInt(token.decimals),
