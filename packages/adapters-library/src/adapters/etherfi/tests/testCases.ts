2a973d0895182e159a44ab47b5d03eea72cc305b
Xavier Brochard <xavier.brochard@consensys.net>
2024-09-16 14:34:41 +0200

    Regen snapshots

--- packages/adapters-library/src/adapters/etherfi/tests/testCases.ts
+++ packages/adapters-library/src/adapters/etherfi/tests/testCases.ts
@@ -8,66 +8,66 @@ const User3 = '0x566d2176Ecb1d8eA07D182b47B5aC57511337E00' // Has weETH on Base
 const User4 = '0x51525Be6985e1B1c46f746a231B1d186B52860DC' // Has weETH on Linea
 
 export const testCases: TestCase[] = [
-  //   {
-  //     key: 'user1',
-  //     chainId: Chain.Ethereum,
-  //     method: 'positions',
+  {
+    key: 'user1',
+    chainId: Chain.Ethereum,
+    method: 'positions',
 
-  //     input: {
-  //       userAddress: User1,
+    input: {
+      userAddress: User1,
 
-  //       filterProtocolTokens: [
-  //         '0x35fA164735182de50811E8e2E824cFb9B6118ac2',
-  //         '0xCd5fE23C85820F7B72D0926FC9b05b43E359b7ee',
-  //       ],
-  //     },
+      filterProtocolTokens: [
+        '0x35fA164735182de50811E8e2E824cFb9B6118ac2',
+        '0xCd5fE23C85820F7B72D0926FC9b05b43E359b7ee',
+      ],
+    },
 
-  //     blockNumber: 20740530,
-  //   },
-  //   {
-  //     key: 'user1',
-  //     chainId: Chain.Ethereum,
-  //     method: 'profits',
+    blockNumber: 20740530,
+  },
+  {
+    key: 'user1',
+    chainId: Chain.Ethereum,
+    method: 'profits',
 
-  //     input: {
-  //       userAddress: User1,
-  //       timePeriod: TimePeriod.sevenDays,
+    input: {
+      userAddress: User1,
+      timePeriod: TimePeriod.sevenDays,
 
-  //       filterProtocolTokens: [
-  //         '0x35fA164735182de50811E8e2E824cFb9B6118ac2',
-  //         '0xCd5fE23C85820F7B72D0926FC9b05b43E359b7ee',
-  //       ],
-  //     },
+      filterProtocolTokens: [
+        '0x35fA164735182de50811E8e2E824cFb9B6118ac2',
+        '0xCd5fE23C85820F7B72D0926FC9b05b43E359b7ee',
+      ],
+    },
 
-  //     blockNumber: 20740544,
-  //   },
-  //   {
-  //     key: 'user2',
-  //     chainId: Chain.Ethereum,
-  //     method: 'positions',
+    blockNumber: 20740544,
+  },
+  {
+    key: 'user2',
+    chainId: Chain.Ethereum,
+    method: 'positions',
 
-  //     input: {
-  //       userAddress: User2,
+    input: {
+      userAddress: User2,
 
-  //       filterProtocolTokens: ['0x7223442cad8e9cA474fC40109ab981608F8c4273'],
-  //     },
+      filterProtocolTokens: ['0x7223442cad8e9cA474fC40109ab981608F8c4273'],
+    },
 
-  //     blockNumber: 20751518,
-  //   },
-  //   {
-  //     key: 'user2',
-  //     chainId: Chain.Ethereum,
-  //     method: 'profits',
+    blockNumber: 20751518,
+  },
+  {
+    key: 'user2',
+    chainId: Chain.Ethereum,
+    method: 'profits',
 
-  //     input: {
-  //       userAddress: User2,
-  //       timePeriod: TimePeriod.thirtyDays,
+    input: {
+      userAddress: User2,
+      timePeriod: TimePeriod.thirtyDays,
 
-  //       filterProtocolTokens: ['0x7223442cad8e9cA474fC40109ab981608F8c4273'],
-  //     },
+      filterProtocolTokens: ['0x7223442cad8e9cA474fC40109ab981608F8c4273'],
+    },
 
-  //     blockNumber: 20751570,
-  //   },
+    blockNumber: 20751570,
+  },
   {
     key: 'user3',
     chainId: Chain.Base,
