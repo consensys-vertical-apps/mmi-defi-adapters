29e4de773183a2706e7c14a40c0418baef5c8f27
JP <jp.sainsbury@consensys.net>
2024-09-17 12:28:56 +0100

    feat: fix

--- packages/adapters-library/src/core/utils/buildIconUrl.ts
+++ packages/adapters-library/src/core/utils/buildIconUrl.ts
@@ -4,6 +4,19 @@ import { ZERO_ADDRESS } from '../constants/ZERO_ADDRESS'
 import { Chain, ChainName } from '../constants/chains'
 import { logger } from './logger'
 
+// names are here https://github.com/trustwallet/assets/tree/master/blockchains
+const chainNameMap: Record<string, string> = {
+  [Chain.Avalanche]: 'avalanchec',
+  [Chain.Polygon]: 'polygon',
+  [Chain.Optimism]: 'optimism',
+  [Chain.Arbitrum]: 'arbitrum',
+  [Chain.Ethereum]: 'ethereum',
+  [Chain.Bsc]: 'binance',
+  [Chain.Fantom]: 'fantom',
+  [Chain.Base]: 'base',
+  [Chain.Linea]: 'linea',
+}
+
 export function buildTrustAssetIconUrl(
   chainId: Chain,
   smartContractAddress: string,
@@ -11,11 +24,7 @@ export function buildTrustAssetIconUrl(
   try {
     const checksumAddress = getAddress(smartContractAddress)
 
-    let chainName = ChainName[chainId]
-
-    if (chainName === ChainName[Chain.Avalanche]) {
-      chainName = 'avalanchec'
-    }
+    const chainName = chainNameMap[chainId]
 
     if (checksumAddress === ZERO_ADDRESS || checksumAddress === E_ADDRESS) {
       return `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/${chainName}/info/logo.png`
