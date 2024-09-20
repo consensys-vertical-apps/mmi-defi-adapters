a9b9072e9a55eb451dd33555f72aadcf9abc528e
Bernardo Garces Chapero <bernardo.chapero@consensys.net>
2024-09-20 13:57:51 +0100

    lint

--- packages/adapters-library/src/defiProvider.ts
+++ packages/adapters-library/src/defiProvider.ts
@@ -47,16 +47,19 @@ import {
 
 import { existsSync } from 'node:fs'
 import {
-  MemoryUnwrapCacheProvider,
   IUnwrapCacheProvider,
+  MemoryUnwrapCacheProvider,
   UnwrapCache,
 } from './unwrapCache'
 
 function buildMetadataProviders(): Record<Chain, IMetadataProvider> {
-  return Object.values(Chain).reduce((acc, chain) => {
-    acc[chain] = new SQLiteMetadataProvider(...dbParams(chain))
-    return acc
-  }, {} as Record<Chain, IMetadataProvider>)
+  return Object.values(Chain).reduce(
+    (acc, chain) => {
+      acc[chain] = new SQLiteMetadataProvider(...dbParams(chain))
+      return acc
+    },
+    {} as Record<Chain, IMetadataProvider>,
+  )
 }
 
 const dbParams = (chainId: Chain): [string, Database.Options] => {
@@ -118,16 +121,19 @@ export class DefiProvider {
         (provider) =>
           !filterChainIds || filterChainIds.includes(provider.chainId),
       )
-      .reduce(async (accumulator, provider) => {
-        if (filterChainIds && !filterChainIds.includes(provider.chainId)) {
-          return accumulator
-        }
+      .reduce(
+        async (accumulator, provider) => {
+          if (filterChainIds && !filterChainIds.includes(provider.chainId)) {
+            return accumulator
+          }
 
-        return {
-          ...(await accumulator),
-          [provider.chainId]: await provider.getStableBlockNumber(),
-        }
-      }, {} as Promise<Partial<Record<Chain, number>>>)
+          return {
+            ...(await accumulator),
+            [provider.chainId]: await provider.getStableBlockNumber(),
+          }
+        },
+        {} as Promise<Partial<Record<Chain, number>>>,
+      )
   }
 
   async getPositions({
@@ -283,9 +289,10 @@ export class DefiProvider {
         return undefined
       }
 
-      const transferLogs = await this.chainProvider.providers[
-        adapter.chainId
-      ].getAllTransferLogsToAddress(userAddress)
+      const transferLogs =
+        await this.chainProvider.providers[
+          adapter.chainId
+        ].getAllTransferLogsToAddress(userAddress)
 
       // no logs on this chain means nothing done on this chain
       if (transferLogs.length === 0) {
