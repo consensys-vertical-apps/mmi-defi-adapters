a0c7b1e6a03e6cfe3e06b8a953b52bd2b0568aa6
JP <jp.sainsbury@consensys.net>
2024-09-30 17:37:30 +0100

    feat: journal

--- packages/adapters-library/src/defiProvider.ts
+++ packages/adapters-library/src/defiProvider.ts
@@ -49,13 +49,10 @@ import { existsSync } from 'node:fs'
 import { IUnwrapCache, IUnwrapCacheProvider, UnwrapCache } from './unwrapCache'
 
 function buildMetadataProviders(): Record<Chain, IMetadataProvider> {
-  return Object.values(Chain).reduce(
-    (acc, chain) => {
-      acc[chain] = new SQLiteMetadataProvider(...dbParams(chain))
-      return acc
-    },
-    {} as Record<Chain, IMetadataProvider>,
-  )
+  return Object.values(Chain).reduce((acc, chain) => {
+    acc[chain] = new SQLiteMetadataProvider(...dbParams(chain))
+    return acc
+  }, {} as Record<Chain, IMetadataProvider>)
 }
 
 const dbParams = (chainId: Chain): [string, Database.Options] => {
@@ -75,7 +72,6 @@ const dbParams = (chainId: Chain): [string, Database.Options] => {
     dbPath,
     {
       fileMustExist: !(process.env.DEFI_ALLOW_DB_CREATION !== 'false'),
-      readonly: true,
     },
   ]
 }
@@ -124,19 +120,16 @@ export class DefiProvider {
         (provider) =>
           !filterChainIds || filterChainIds.includes(provider.chainId),
       )
-      .reduce(
-        async (accumulator, provider) => {
-          if (filterChainIds && !filterChainIds.includes(provider.chainId)) {
-            return accumulator
-          }
+      .reduce(async (accumulator, provider) => {
+        if (filterChainIds && !filterChainIds.includes(provider.chainId)) {
+          return accumulator
+        }
 
-          return {
-            ...(await accumulator),
-            [provider.chainId]: await provider.getStableBlockNumber(),
-          }
-        },
-        {} as Promise<Partial<Record<Chain, number>>>,
-      )
+        return {
+          ...(await accumulator),
+          [provider.chainId]: await provider.getStableBlockNumber(),
+        }
+      }, {} as Promise<Partial<Record<Chain, number>>>)
   }
 
   async getPositions({
@@ -292,10 +285,9 @@ export class DefiProvider {
         return undefined
       }
 
-      const transferLogs =
-        await this.chainProvider.providers[
-          adapter.chainId
-        ].getAllTransferLogsToAddress(userAddress)
+      const transferLogs = await this.chainProvider.providers[
+        adapter.chainId
+      ].getAllTransferLogsToAddress(userAddress)
 
       // no logs on this chain means nothing done on this chain
       if (transferLogs.length === 0) {
