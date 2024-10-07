dda9fba57264a4cdb8a10c2df4fcf262e5ba0975
JP <jp.sainsbury@consensys.net>
2024-10-07 15:54:11 +0100

    feat: fix borrow

--- packages/adapters-library/src/adapters/mendi-finance/products/borrow-market/mendiFinanceBorrowMarketAdapter.ts
+++ packages/adapters-library/src/adapters/mendi-finance/products/borrow-market/mendiFinanceBorrowMarketAdapter.ts
@@ -22,34 +22,15 @@ import {
 import { Protocol } from '../../../protocols'
 import { GetTransactionParams } from '../../../supportedProtocols'
 import { Cerc20__factory, Comptroller__factory } from '../../contracts'
+import { CompoundV2BorrowMarketForkAdapter } from '../../../../core/adapters/CompoundV2BorrowMarketForkAdapter'
+import { contractAddresses } from '../supply-market/mendiFinanceSupplyMarketAdapter'
 
-const contractAddresses: Partial<
-  Record<
-    Chain,
-    {
-      comptroller: string
-      speed: string
-      oracle: string
-      velocore: string
-      converter: string
-      mendi: string
-      usdcE: string
-    }
-  >
-> = {
-  [Chain.Linea]: {
-    comptroller: getAddress('0x1b4d3b0421dDc1eB216D230Bc01527422Fb93103'),
-    speed: getAddress('0x3b9B9364Bf69761d308145371c38D9b558013d40'),
-    oracle: getAddress('0xCcBea2d7e074744ab46e28a043F85038bCcfFec2'),
-    velocore: getAddress('0xaA18cDb16a4DD88a59f4c2f45b5c91d009549e06'),
-    converter: getAddress('0xAADAa473C1bDF7317ec07c915680Af29DeBfdCb5'),
-    mendi: getAddress('0x43E8809ea748EFf3204ee01F08872F063e44065f'),
-    usdcE: getAddress('0x176211869cA2b568f2A7D4EE941E073a821EE1ff'),
-  },
-}
-
-export class MendiFinanceBorrowMarketAdapter extends SimplePoolAdapter {
+export class MendiFinanceBorrowMarketAdapter extends CompoundV2BorrowMarketForkAdapter {
   productId = 'borrow-market'
+
+  contractAddresses: Partial<
+    Record<Chain, { comptrollerAddress: string; cUSDCv3Address: string }>
+  > = contractAddresses
 
   adapterSettings = {
     enablePositionDetectionByProtocolTokenTransfer: false,
@@ -68,110 +49,4 @@ export class MendiFinanceBorrowMarketAdapter extends SimplePoolAdapter {
       productId: this.productId,
     }
   }
-
-  @CacheToDb
-  async getProtocolTokens(): Promise<ProtocolToken[]> {
-    const comptrollerContract = Comptroller__factory.connect(
-      contractAddresses[this.chainId]!.comptroller,
-      this.provider,
-    )
-
-    const pools = await comptrollerContract.getAllMarkets()
-
-    const metadataObject: ProtocolToken[] = []
-
-    await Promise.all(
-      pools.map(async (poolContractAddress) => {
-        const poolContract = Cerc20__factory.connect(
-          poolContractAddress,
-          this.provider,
-        )
-
-        let underlyingContractAddress: string
-        try {
-          underlyingContractAddress = await poolContract.underlying()
-        } catch (error) {
-          underlyingContractAddress = ZERO_ADDRESS
-        }
-
-        const protocolTokenPromise = getTokenMetadata(
-          poolContractAddress,
-          this.chainId,
-          this.provider,
-        )
-        const underlyingTokenPromise = getTokenMetadata(
-          underlyingContractAddress,
-          this.chainId,
-          this.provider,
-        )
-
-        const [protocolToken, underlyingToken] = await Promise.all([
-          protocolTokenPromise,
-          underlyingTokenPromise,
-        ])
-
-        metadataObject.push({
-          ...protocolToken,
-          underlyingTokens: [underlyingToken],
-        })
-      }),
-    )
-    return metadataObject
-  }
-
-  async getTotalValueLocked(
-    _input: GetTotalValueLockedInput,
-  ): Promise<ProtocolTokenTvl[]> {
-    throw new NotImplementedError()
-  }
-
-  protected async unwrapProtocolToken(
-    _protocolTokenMetadata: Erc20Metadata,
-    _blockNumber?: number | undefined,
-  ): Promise<UnwrappedTokenExchangeRate[]> {
-    throw new NotImplementedError()
-  }
-
-  async getTransactionParams({
-    action,
-    inputs,
-  }: Extract<
-    GetTransactionParams,
-    { protocolId: typeof Protocol.MendiFinance; productId: 'borrow-market' }
-  >): Promise<{ to: string; data: string }> {
-    const assetPool = Object.values(await this.getProtocolTokens()).find(
-      (pool) => pool.address === inputs.asset,
-    )
-
-    if (!assetPool) {
-      throw new Error('Asset pool not found')
-    }
-
-    const poolContract = Cerc20__factory.connect(
-      assetPool.address,
-      this.provider,
-    )
-
-    const { amount } = inputs
-
-    switch (action) {
-      case WriteActions.Borrow: {
-        return poolContract.borrow.populateTransaction(amount)
-      }
-      case WriteActions.Repay: {
-        return poolContract.repayBorrow.populateTransaction(amount)
-      }
-    }
-  }
 }
-
-export const WriteActionInputs = {
-  [WriteActions.Borrow]: z.object({
-    asset: z.string(),
-    amount: z.string(),
-  }),
-  [WriteActions.Repay]: z.object({
-    asset: z.string(),
-    amount: z.string(),
-  }),
-} satisfies WriteActionInputSchemas
--- packages/adapters-library/src/adapters/mendi-finance/products/supply-market/mendiFinanceSupplyMarketAdapter.ts
+++ packages/adapters-library/src/adapters/mendi-finance/products/supply-market/mendiFinanceSupplyMarketAdapter.ts
@@ -20,39 +20,48 @@ import {
 import { Protocol } from '../../../protocols'
 import { GetTransactionParams } from '../../../supportedProtocols'
 import { Cerc20__factory, Comptroller__factory } from '../../contracts'
+import { CompoundV2SupplyMarketForkAdapter } from '../../../../core/adapters/CompoundV2SupplyMarketForkAdapter'
 
-const contractAddresses: Partial<
+export const contractAddresses: Partial<
   Record<
     Chain,
     {
-      comptroller: string
+      comptrollerAddress: string
       speed: string
       oracle: string
       velocore: string
       converter: string
       mendi: string
       usdcE: string
+      cUSDCv3Address: string
     }
   >
 > = {
   [Chain.Linea]: {
-    comptroller: getAddress('0x1b4d3b0421dDc1eB216D230Bc01527422Fb93103'),
+    comptrollerAddress: getAddress(
+      '0x1b4d3b0421dDc1eB216D230Bc01527422Fb93103',
+    ),
     speed: getAddress('0x3b9B9364Bf69761d308145371c38D9b558013d40'),
     oracle: getAddress('0xCcBea2d7e074744ab46e28a043F85038bCcfFec2'),
     velocore: getAddress('0xaA18cDb16a4DD88a59f4c2f45b5c91d009549e06'),
     converter: getAddress('0xAADAa473C1bDF7317ec07c915680Af29DeBfdCb5'),
     mendi: getAddress('0x43E8809ea748EFf3204ee01F08872F063e44065f'),
     usdcE: getAddress('0x176211869cA2b568f2A7D4EE941E073a821EE1ff'),
+    cUSDCv3Address: '0x', // compound version 3 transaction params
   },
 }
 
-export class MendiFinanceSupplyMarketAdapter extends SimplePoolAdapter {
+export class MendiFinanceSupplyMarketAdapter extends CompoundV2SupplyMarketForkAdapter {
   productId = 'supply-market'
 
   adapterSettings = {
     enablePositionDetectionByProtocolTokenTransfer: true,
     includeInUnwrap: true,
   }
+
+  contractAddresses: Partial<
+    Record<Chain, { comptrollerAddress: string; cUSDCv3Address: string }>
+  > = contractAddresses
 
   getProtocolDetails(): ProtocolDetails {
     return {
@@ -66,130 +75,4 @@ export class MendiFinanceSupplyMarketAdapter extends SimplePoolAdapter {
       productId: this.productId,
     }
   }
-
-  @CacheToDb
-  async getProtocolTokens(): Promise<ProtocolToken[]> {
-    const comptrollerContract = Comptroller__factory.connect(
-      contractAddresses[this.chainId]!.comptroller,
-      this.provider,
-    )
-
-    const pools = await comptrollerContract.getAllMarkets()
-
-    const metadataObject: ProtocolToken[] = []
-
-    await Promise.all(
-      pools.map(async (poolContractAddress) => {
-        const poolContract = Cerc20__factory.connect(
-          poolContractAddress,
-          this.provider,
-        )
-
-        let underlyingContractAddress: string
-        try {
-          underlyingContractAddress = await poolContract.underlying()
-        } catch (error) {
-          underlyingContractAddress = ZERO_ADDRESS
-        }
-
-        const protocolTokenPromise = getTokenMetadata(
-          poolContractAddress,
-          this.chainId,
-          this.provider,
-        )
-        const underlyingTokenPromise = getTokenMetadata(
-          underlyingContractAddress,
-          this.chainId,
-          this.provider,
-        )
-
-        const [protocolToken, underlyingToken] = await Promise.all([
-          protocolTokenPromise,
-          underlyingTokenPromise,
-        ])
-
-        metadataObject.push({
-          ...protocolToken,
-          underlyingTokens: [underlyingToken],
-        })
-      }),
-    )
-    return metadataObject
-  }
-
-  protected async unwrapProtocolToken(
-    protocolTokenMetadata: Erc20Metadata,
-    blockNumber?: number | undefined,
-  ): Promise<UnwrappedTokenExchangeRate[]> {
-    const underlyingTokens = await this.fetchUnderlyingTokensMetadata(
-      protocolTokenMetadata.address,
-    )
-
-    const poolContract = Cerc20__factory.connect(
-      protocolTokenMetadata.address,
-      this.provider,
-    )
-
-    const exchangeRateCurrent =
-      await poolContract.exchangeRateCurrent.staticCall({
-        blockTag: blockNumber,
-      })
-
-    // The current exchange rate is scaled by 1 * 10^(18 - 8 + Underlying Token Decimals).
-    const adjustedExchangeRate = exchangeRateCurrent / 10n ** 10n
-
-    return [
-      {
-        ...underlyingTokens[0]!,
-        type: TokenType.Underlying,
-        underlyingRateRaw: adjustedExchangeRate,
-      },
-    ]
-  }
-
-  async getTransactionParams({
-    action,
-    inputs,
-  }: Extract<
-    GetTransactionParams,
-    { protocolId: typeof Protocol.MendiFinance; productId: 'supply-market' }
-  >): Promise<{ to: string; data: string }> {
-    const assetPool = Object.values(await this.getProtocolTokens()).find(
-      (pool) => pool.underlyingTokens[0]!.address === inputs.asset,
-    )
-
-    if (!assetPool) {
-      throw new Error('Asset pool not found')
-    }
-
-    const poolContract = Cerc20__factory.connect(
-      assetPool.address,
-      this.provider,
-    )
-
-    const { amount } = inputs
-
-    switch (action) {
-      case WriteActions.Deposit: {
-        return poolContract.mint.populateTransaction(amount)
-      }
-      case WriteActions.Withdraw: {
-        return poolContract.redeem.populateTransaction(amount)
-      }
-      default: {
-        throw new Error('Invalid action')
-      }
-    }
-  }
 }
-
-export const WriteActionInputs = {
-  [WriteActions.Deposit]: z.object({
-    asset: z.string(),
-    amount: z.string(),
-  }),
-  [WriteActions.Withdraw]: z.object({
-    asset: z.string(),
-    amount: z.string(),
-  }),
-} satisfies WriteActionInputSchemas
