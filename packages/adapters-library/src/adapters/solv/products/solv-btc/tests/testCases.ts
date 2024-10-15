b6ff0afcb1d0be2d2864f3a9770740bc69f16b04
Bernardo Garces Chapero <bernardo.chapero@consensys.net>
2024-10-15 16:59:37 +0100

    solv

--- packages/adapters-library/src/adapters/solv/products/solv-btc/tests/testCases.ts
+++ packages/adapters-library/src/adapters/solv/products/solv-btc/tests/testCases.ts
@@ -4,9 +4,7 @@ import type { TestCase } from '../../../../../types/testCase'
 
 const User1 = '0x7BFEe91193d9Df2Ac0bFe90191D40F23c773C060' // Has some SolvBTC on Arbitrum
 const User2 = '0xEBFAEEDE1D85E8E87BDe9326bc301830D55dfa8c' // Has some SolvBTC.BBN on Mainnet
-// const User3 = '0x423e5E0ee2615E6bef4B181400553066dAE3b6fD' // Has some SolvBTC.ENA on BSC
 const User4 = '0xd87D6D2D766b15cDA45e3cACC8742104B5A921ea' // Has deposited + withdrew some SolvBTC over a few days
-const User5 = '0xabef19a5Cb082D0e512eB28363B1229B25BaB9a7' // Has Holdings and Redemptions in some yield markets on Arbitrum
 
 export const testCases: TestCase[] = [
   {
@@ -82,78 +80,5 @@ export const testCases: TestCase[] = [
     },
 
     blockNumber: 243434212,
-  },
-  // {
-  //   key: 'user3',
-  //   chainId: Chain.Bsc,
-  //   method: 'positions',
-
-  //   input: {
-  //     userAddress: User3,
-  //     filterProtocolTokens: ['0x53E63a31fD1077f949204b94F431bCaB98F72BCE'],
-  //   },
-
-  //   blockNumber: 41411562,
-  // },
-  {
-    key: 'user5',
-    chainId: Chain.Arbitrum,
-    method: 'positions',
-
-    input: {
-      userAddress: User5,
-
-      filterProtocolTokens: [
-        '0x22799daa45209338b7f938edf251bdfd1e6dcb32',
-        '0x22799daa45209338b7f938edf251bdfd1e6dcb32',
-        '0x22799daa45209338b7f938edf251bdfd1e6dcb32',
-        '0xe9bd233b2b34934fb83955ec15c2ac48f31a0e8c',
-        '0xe9bd233b2b34934fb83955ec15c2ac48f31a0e8c',
-      ],
-
-      filterTokenIds: ['10334', '10484', '10566', '3136', '3137'],
-    },
-
-    blockNumber: 245491037,
-  },
-  // {
-  //   key: 'user5',
-  //   chainId: Chain.Bsc,
-  //   method: 'positions',
-
-  //   input: {
-  //     userAddress: User5,
-
-  //     filterProtocolTokens: [
-  //       '0xb816018e5d421e8b809a4dc01af179d86056ebdf',
-  //       '0xe16cec2f385ea7a382772334a44506a865f98562',
-  //     ],
-
-  //     filterTokenIds: ['38288', '138'],
-  //   },
-
-  //   blockNumber: 41584905,
-  // },
-  {
-    key: 'user5',
-    chainId: Chain.Arbitrum,
-    method: 'profits',
-
-    input: {
-      userAddress: User5,
-      timePeriod: TimePeriod.thirtyDays,
-
-      filterProtocolTokens: [
-        '0xe9bD233b2b34934Fb83955EC15c2ac48F31A0E8c',
-        '0xe9bD233b2b34934Fb83955EC15c2ac48F31A0E8c',
-        '0x22799DAA45209338B7f938edf251bdfD1E6dCB32',
-        '0x22799DAA45209338B7f938edf251bdfD1E6dCB32',
-        '0x22799DAA45209338B7f938edf251bdfD1E6dCB32',
-      ],
-
-      filterTokenIds: ['3136', '3137', '10334', '10484', '10566'],
-    },
-
-    blockNumber: 246945289,
   },
 ]
