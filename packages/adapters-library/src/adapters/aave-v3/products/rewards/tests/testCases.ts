b0f031bd6f1d6c709b345b0c7a9e0e4d404d3994
JP <jp.sainsbury@consensys.net>
2024-10-16 16:07:20 +0100

    feat: aave

--- packages/adapters-library/src/adapters/aave-v3/products/rewards/tests/testCases.ts
+++ packages/adapters-library/src/adapters/aave-v3/products/rewards/tests/testCases.ts
@@ -5,48 +5,15 @@ import { WriteActions } from '../../../../../types/writeActions'
 
 export const testCases: TestCase[] = [
   {
-    chainId: Chain.Ethereum,
+    chainId: Chain.Arbitrum,
     method: 'positions',
+    key: 'reward',
 
     input: {
-      userAddress: '0x4f575BDdc36c3Ec42D923AEeEc4Ada1a60ce4086',
-
-      filterProtocolTokens: [
-        '0x4d5F47FA6A74757f35C14fD3a6Ef8E3C9BC514E8',
-        '0x6df1C1E379bC5a00a7b4C6e67A203333772f45A8',
-      ],
+      userAddress: '0x5d14d2fc18f592b0fe5f6ce1ae091380294dcf71',
+      filterProtocolTokens: ['0x929EC64c34a17401F460460D4B9390518E5B473e'],
     },
 
-    blockNumber: 19818581,
-  },
-  {
-    chainId: Chain.Ethereum,
-    method: 'profits',
-
-    input: {
-      userAddress: '0xee46ee0dE21937772291a006c3541aFa557dc9B8',
-      timePeriod: TimePeriod.oneDay,
-
-      filterProtocolTokens: [
-        '0x5Ee5bf7ae06D1Be5997A1A72006FE6C607eC6DE8',
-        '0x5E8C8A7243651DB1384C0dDfDbE39761E8e7E51a',
-        '0x23878914EFE38d27C4D67Ab83ed1b93A74D4086a',
-        '0xeA51d7853EEFb32b6ee06b1C12E6dcCA88Be0fFE',
-      ],
-    },
-
-    blockNumber: 19818884,
-  },
-  {
-    chainId: Chain.Ethereum,
-    method: 'tvl',
-
-    filterProtocolTokens: [
-      '0x4d5F47FA6A74757f35C14fD3a6Ef8E3C9BC514E8',
-      '0x0B925eD163218f6662a35e0f0371Ac234f9E9371',
-      '0x98C23E9d8f34FEFb1B7BD6a91B7FF122F4e16F5c',
-    ],
-
-    blockNumber: 19818582,
+    blockNumber: 264107941,
   },
 ]
