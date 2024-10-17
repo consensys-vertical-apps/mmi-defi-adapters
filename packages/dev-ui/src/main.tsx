357cf4753d1698d375403fd0c276b67d746559ac
Bernardo Garces Chapero <bernardo.chapero@consensys.net>
2024-10-17 15:55:20 +0100

    linting

--- packages/dev-ui/src/main.tsx
+++ packages/dev-ui/src/main.tsx
@@ -1,8 +1,8 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import './index.css'
 
 const queryClient = new QueryClient()
