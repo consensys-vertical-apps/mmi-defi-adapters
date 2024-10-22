ab7f117145f68599860f28f7bf558b41b23da59a
JP <jp.sainsbury@consensys.net>
2024-10-22 09:54:45 +0100

    feat: aave

--- packages/adapters-library/src/adapters/aave-v3/products/stable-debt-token/tests/testCases.ts
+++ packages/adapters-library/src/adapters/aave-v3/products/stable-debt-token/tests/testCases.ts
@@ -3,4 +3,12 @@ import { TimePeriod } from '../../../../../core/constants/timePeriod'
 import type { TestCase } from '../../../../../types/testCase'
 import { WriteActions } from '../../../../../types/writeActions'
 
-export const testCases: TestCase[] = []
+export const testCases: TestCase[] = [
+  {
+    chainId: Chain.Ethereum,
+    method: 'tvl',
+    key: 'tvl',
+    filterProtocolTokens: ['0x63B1129ca97D2b9F97f45670787Ac12a9dF1110a'],
+    blockNumber: 21020002,
+  },
+]
