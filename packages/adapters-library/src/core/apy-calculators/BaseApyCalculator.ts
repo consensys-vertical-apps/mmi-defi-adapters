6314053b80e73cbd03fcb2f34cf573d0fbff1b84
Xavier Brochard <xavier.brochard@consensys.net>
2024-10-31 16:26:09 +0100

    test: fix unit tests

--- packages/adapters-library/src/core/apy-calculators/BaseApyCalculator.ts
+++ packages/adapters-library/src/core/apy-calculators/BaseApyCalculator.ts
@@ -1,7 +1,7 @@
 import { AVERAGE_BLOCKS_PER_DAY } from '../constants/AVERAGE_BLOCKS_PER_DAY'
+import { InvalidArgumentError } from '../errors/errors'
 import { ApyCalculator, ApyInfo, GetApyArgs } from './ApyCalculator'
 import { VoidApyCalculator } from './VoidApyCalculator'
-import { computeApr, computeApy } from './helpers'
 
 /**
  * An abstract APY calculator that handles logic that is common to all APY calculators.
@@ -26,10 +26,10 @@ export abstract class BaseApyCalculator implements ApyCalculator {
 
       const interestPercent = interest * 100
 
-      const apr = computeApr(interest, frequency)
+      const apr = this.computeApr(interest, frequency)
       const aprPercent = apr * 100
 
-      const apy = computeApy(apr, frequency)
+      const apy = this.computeApy(apr, frequency)
       const apyPercent = apy * 100
 
       return {
@@ -61,4 +61,42 @@ export abstract class BaseApyCalculator implements ApyCalculator {
    * Implement this method in the extending class.
    */
   protected abstract computeInterest(args: GetApyArgs): number
+
+  /**
+   * Computes the Annual Percentage Yield (APY) given the Annual Percentage Rate (APR) and the frequency of compounding periods.
+   *
+   * APY takes into account the effect of interest compounding over a year, showing the real return on an investment.
+   *
+   * @param {number} apr - The Annual Percentage Rate (APR) as a decimal (e.g., 0.05 for 5% APR).
+   * @param {number} frequency - The number of compounding periods in a year (e.g., 12 for monthly, 365 for daily).
+   * @returns {number} The computed APY, which is the annual yield after accounting for compounding.
+   *
+   * @example
+   * // Monthly compounding, 6% APR
+   * const apy = computeApy(0.06, 12); // ~ 0.0618 ~ (6.18%)
+   */
+  public computeApy(apr: number, frequency: number) {
+    if (frequency === 0)
+      throw new InvalidArgumentError(
+        'Frequency cannot be 0 as it would result in division by zero.',
+      )
+    return Math.pow(1 + apr / frequency, frequency) - 1
+  }
+
+  /**
+   * Computes the Annual Percentage Rate (APR) given the interest earned and the frequency of compounding periods.
+   *
+   * APR represents the simple interest rate over a period without considering compounding.
+   *
+   * @param {number} interest - The interest earned in each compounding period (as a decimal).
+   * @param {number} frequency - The number of compounding periods in a year (e.g., 12 for monthly, 365 for daily).
+   * @returns {number} The computed APR, which is the annualized simple interest rate.
+   *
+   * @example
+   * // Month period, 0.5% interest per period
+   * const apr = computeApr(0.005, 12); // 0.06 = 6%
+   */
+  public computeApr(interest: number, frequency: number) {
+    return frequency * interest
+  }
 }
