733a1a7a5cd34504b3837500cc67b157886a7714
JP <jp.sainsbury@consensys.net>
2024-12-09 14:39:41 +0000

    feat: beefy fix

--- packages/adapters-library/src/adapters/beefy/products/moo-token/beefyMooTokenAdapter.ts
+++ packages/adapters-library/src/adapters/beefy/products/moo-token/beefyMooTokenAdapter.ts
@@ -23,21 +23,18 @@ import {
   TokenType,
   UnwrapExchangeRate,
   UnwrapInput,
+  UnwrappedTokenExchangeRate,
 } from '../../../../types/adapter'
 import { Erc20Metadata } from '../../../../types/erc20Metadata'
 import { Protocol } from '../../../protocols'
-import { breakdownFetcherMap, chainIdMap, protocolMap } from '../../sdk/config'
+import { BeefyVaultV7__factory } from '../../contracts'
+import { chainIdMap } from '../../sdk/config'
 import {
   ApiClmManager,
   ApiVault,
   BeefyProductType,
   ProtocolUnwrapType,
 } from '../../sdk/types'
-
-type AdditionalMetadata = {
-  unwrapType: ProtocolUnwrapType
-  underlyingLPToken: Erc20Metadata
-}
 
 export class BeefyMooTokenAdapter implements IProtocolAdapter {
   productId = BeefyProductType.MOO_TOKEN
@@ -82,7 +79,7 @@ export class BeefyMooTokenAdapter implements IProtocolAdapter {
   }
 
   @CacheToDb
-  async getProtocolTokens(): Promise<ProtocolToken<AdditionalMetadata>[]> {
+  async getProtocolTokens(): Promise<ProtocolToken[]> {
     const chain = chainIdMap[this.chainId]
 
     const cowTokenAddresses = await fetch(
@@ -111,87 +108,85 @@ export class BeefyMooTokenAdapter implements IProtocolAdapter {
 
     // for each vault, get the latest breakdown to get the token list
     return await filterMapAsync(vaults, async (vault) => {
-      const platformConfig = protocolMap[vault.platformId]
-      const protocolType =
-        vault.tokenAddress &&
-        cowTokenAddresses.has(vault.tokenAddress.toLocaleLowerCase())
-          ? 'beefy_clm'
-          : typeof platformConfig === 'string' || !platformConfig
-            ? platformConfig
-            : platformConfig[vault.strategyTypeId || 'default']
-
-      if (!protocolType) {
-        logger.debug(
-          {
-            productId: this.productId,
-            vaultId: vault.id,
-            platformId: vault.platformId,
-            vaultAddress: vault.earnedTokenAddress,
-            poolAddress: vault.tokenAddress,
-            strategyTypeId: vault.strategyTypeId,
-            chain,
-          },
-          'Protocol type not found',
-        )
-        return undefined
-      }
-
-      // test that we can indeed fetch the breakdown, otherwise we don't include the vault
-      // in the list
-      const [protocolToken, underlyingToken, breakdown] = await Promise.all([
-        this.helpers.getTokenMetadata(vault.earnedTokenAddress),
-        this.helpers.getTokenMetadata(vault.tokenAddress),
-        breakdownFetcherMap[protocolType](
-          {
-            protocolTokenAddress: vault.earnedTokenAddress,
-            underlyingLPTokenAddress: vault.tokenAddress,
-            blockSpec: { blockTag: undefined },
-          },
-          this.provider,
-        ),
-      ])
-
-      const breakdownTokenMetadata = await Promise.all(
-        breakdown.balances.map((balance) =>
-          this.helpers.getTokenMetadata(balance.tokenAddress),
-        ),
+      if (
+        vault.earnedTokenAddress !==
+        '0x4D75a9342113c106F48117d81e2952A5828d1B5F'
       )
+        return
+      try {
+        const [protocolToken, underlyingToken] = await Promise.all([
+          this.helpers.getTokenMetadata(vault.earnedTokenAddress),
+          this.helpers.getTokenMetadata(vault.tokenAddress),
+        ])
 
-      return {
-        ...protocolToken,
-        underlyingTokens: breakdownTokenMetadata,
-        underlyingLPToken: underlyingToken,
-        unwrapType: protocolType,
+        return {
+          ...protocolToken,
+          underlyingTokens: [underlyingToken],
+        }
+      } catch (error) {
+        return
       }
     })
   }
 
   async unwrap({
-    protocolTokenAddress,
     blockNumber,
+    protocolTokenAddress,
   }: UnwrapInput): Promise<UnwrapExchangeRate> {
-    const {
-      underlyingTokens,
-      unwrapType,
-      underlyingLPToken,
-      ...protocolToken
-    } = await this.getProtocolTokenByAddress(protocolTokenAddress)
+    const protocolTokenMetadata =
+      await this.getProtocolTokenByAddress(protocolTokenAddress)
+
+    const underlyingTokenConversionRate = await this.unwrapProtocolToken(
+      protocolTokenMetadata,
+      blockNumber,
+    )
 
     return {
-      ...protocolToken,
+      ...protocolTokenMetadata,
       baseRate: 1,
-      type: TokenType['Protocol'],
-      tokens: [
-        {
-          ...underlyingLPToken,
-          underlyingRateRaw: BigInt(10 ** underlyingLPToken.decimals),
-          type: TokenType['Underlying'],
-        },
-      ],
+      type: TokenType.Protocol,
+      tokens: underlyingTokenConversionRate,
     }
   }
 
+  protected async unwrapProtocolToken(
+    protocolTokenMetadata: Erc20Metadata,
+    blockNumber?: number | undefined,
+  ): Promise<UnwrappedTokenExchangeRate[]> {
+    const [underlyingToken] = await this.getUnderlyingTokens(
+      protocolTokenMetadata.address,
+    )
+
+    const wstEthContract = BeefyVaultV7__factory.connect(
+      protocolTokenMetadata.address,
+      this.provider,
+    )
+
+    const pricePerShareRaw = await wstEthContract.getPricePerFullShare({
+      blockTag: blockNumber,
+    })
+
+    return [
+      {
+        ...underlyingToken!,
+        type: TokenType.Underlying,
+        underlyingRateRaw: pricePerShareRaw,
+      },
+    ]
+  }
+
+  protected async getUnderlyingTokens(
+    protocolTokenAddress: string,
+  ): Promise<Erc20Metadata[]> {
+    const { underlyingTokens } =
+      await this.getProtocolTokenByAddress(protocolTokenAddress)
+
+    return underlyingTokens
+  }
+
   async getPositions(input: GetPositionsInput): Promise<ProtocolPosition[]> {
+    console.log('getPositions', await this.getProtocolTokens(), this.chainId)
+
     return this.helpers.getBalanceOfTokens({
       ...input,
       protocolTokens: await this.getProtocolTokens(),
@@ -237,7 +232,7 @@ export class BeefyMooTokenAdapter implements IProtocolAdapter {
 
   private async getProtocolTokenByAddress(
     protocolTokenAddress: string,
-  ): Promise<ProtocolToken<AdditionalMetadata>> {
+  ): Promise<ProtocolToken> {
     return this.helpers.getProtocolTokenByAddress({
       protocolTokens: await this.getProtocolTokens(),
       protocolTokenAddress,
---
+++ packages/adapters-library/src/scripts/deleteAdapterMetadata.ts
@@ -0,0 +1,205 @@
+import fs from 'node:fs'
+import path from 'node:path'
+import Database, { Database as DatabaseType } from 'better-sqlite3'
+import chalk from 'chalk'
+import { Command } from 'commander'
+import { Protocol } from '../adapters/protocols'
+import { supportedProtocols } from '../adapters/supportedProtocols'
+import { AdaptersController } from '../core/adaptersController'
+import { ZERO_ADDRESS } from '../core/constants/ZERO_ADDRESS'
+import { Chain, ChainIdToChainNameMap } from '../core/constants/chains'
+import { ProviderMissingError } from '../core/errors/errors'
+import { CustomJsonRpcProvider } from '../core/provider/CustomJsonRpcProvider'
+import { filterMapSync } from '../core/utils/filters'
+import { logger } from '../core/utils/logger'
+import {
+  AdditionalMetadataWithReservedFields,
+  Erc20ExtendedMetadata,
+  IProtocolAdapter,
+  ProtocolToken,
+} from '../types/IProtocolAdapter'
+import { Erc20Metadata } from '../types/erc20Metadata'
+import { getInvalidAddresses } from './addressValidation'
+import { multiChainFilter, multiProtocolFilter } from './commandFilters'
+
+export function deleteAdapterMetadata(
+  program: Command,
+  chainProviders: Record<Chain, CustomJsonRpcProvider>,
+  adaptersController: AdaptersController,
+) {
+  program
+    .command('delete-adapter-metadata')
+    .option(
+      '-p, --protocols <protocols>',
+      'protocol id (e.g. stargate or aave-v2)',
+    )
+    .option(
+      '-pd, --products <products>',
+      'product id (e.g. voting-escrow or market-borrow)',
+    )
+    .option(
+      '-c, --chains <chains>',
+      'chains id or name (e.g. ethereum,arbitrum,linea)',
+    )
+    .showHelpAfterError()
+    .action(
+      async ({
+        protocols,
+        products,
+        chains,
+      }: { protocols: string; products: string; chains: string }) => {
+
+        console.log('Starting deletion process...')
+        console.log('Protocols:', protocols)
+        console.log('Products:', products)
+        console.log('Chains:', chains)
+
+        const filterProtocolIds = multiProtocolFilter(protocols)!
+        if (!protocols || filterProtocolIds && filterProtocolIds.length !== 1) {
+          throw new Error('One protocol must be supplied at a time')
+        }
+        const protocolId = filterProtocolIds.pop()!
+
+        const filterProductIds = products?.split(',')
+        if (!products || filterProductIds && filterProductIds.length !== 1) {
+          throw new Error('One product must be supplied at a time')
+        }
+        const productId = filterProductIds?.pop()!
+
+        const filterChainIds = multiChainFilter(chains)!
+        if (!chains || filterChainIds && filterChainIds.length !== 1) {
+          throw new Error('One chain must be supplied at a time')
+        }
+        const chainId = filterChainIds.pop()!
+
+        const db = new Database(`./${ChainIdToChainNameMap[chainId]}.db`, {
+          verbose: console.log,
+        })
+
+        // Deleting rows in underlying_tokens
+        checkAndDelete(
+          db,
+          `
+  SELECT * FROM underlying_tokens
+  WHERE pool_id IN (
+      SELECT p.pool_id
+      FROM adapters a
+      JOIN pools p ON p.adapter_id = a.adapter_id
+      WHERE a.protocol_id = ? AND a.product_id = ?
+  )
+  `,
+          `
+  DELETE FROM underlying_tokens
+  WHERE pool_id IN (
+      SELECT p.pool_id
+      FROM adapters a
+      JOIN pools p ON p.adapter_id = a.adapter_id
+      WHERE a.protocol_id = ? AND a.product_id = ?
+  )
+  `,
+          [protocolId, productId],
+        )
+
+        // Deleting rows in reward_tokens
+        checkAndDelete(
+          db,
+          `
+  SELECT * FROM reward_tokens
+  WHERE pool_id IN (
+      SELECT p.pool_id
+      FROM adapters a
+      JOIN pools p ON p.adapter_id = a.adapter_id
+      WHERE a.protocol_id = ? AND a.product_id = ?
+  )
+  `,
+          `
+  DELETE FROM reward_tokens
+  WHERE pool_id IN (
+      SELECT p.pool_id
+      FROM adapters a
+      JOIN pools p ON p.adapter_id = a.adapter_id
+      WHERE a.protocol_id = ? AND a.product_id = ?
+  )
+  `,
+          [protocolId, productId],
+        )
+
+        // Deleting rows in pools
+        checkAndDelete(
+          db,
+          `
+  SELECT * FROM pools
+  WHERE pool_address IN (
+      SELECT p.pool_address
+      FROM adapters a
+      JOIN pools p ON p.adapter_id = a.adapter_id
+      WHERE a.protocol_id = ? AND a.product_id = ?
+  )
+  `,
+          `
+  DELETE FROM pools
+  WHERE pool_address IN (
+      SELECT p.pool_address
+      FROM adapters a
+      JOIN pools p ON p.adapter_id = a.adapter_id
+      WHERE a.protocol_id = ? AND a.product_id = ?
+  )
+  `,
+          [protocolId, productId],
+        )
+
+        // Deleting rows in tokens
+        checkAndDelete(
+          db,
+          `
+  SELECT * FROM tokens
+  WHERE token_address IS NULL OR token_address IN (
+      SELECT t.token_address
+      FROM tokens t
+      WHERE NOT EXISTS (SELECT 1 FROM pools WHERE pool_address = t.token_address)
+        AND NOT EXISTS (SELECT 1 FROM underlying_tokens WHERE token_address = t.token_address)
+        AND NOT EXISTS (SELECT 1 FROM reward_tokens WHERE token_address = t.token_address)
+        AND NOT EXISTS (SELECT 1 FROM extra_reward_tokens WHERE token_address = t.token_address)
+  )
+  `,
+          `
+  DELETE FROM tokens
+  WHERE token_address IS NULL OR token_address IN (
+      SELECT t.token_address
+      FROM tokens t
+      WHERE NOT EXISTS (SELECT 1 FROM pools WHERE pool_address = t.token_address)
+        AND NOT EXISTS (SELECT 1 FROM underlying_tokens WHERE token_address = t.token_address)
+        AND NOT EXISTS (SELECT 1 FROM reward_tokens WHERE token_address = t.token_address)
+        AND NOT EXISTS (SELECT 1 FROM extra_reward_tokens WHERE token_address = t.token_address)
+  )
+  `,
+          [],
+        )
+
+        console.log('Deletion process completed.')
+
+        db.close()
+      },
+    )
+}
+
+// Utility function to log rows to be deleted and verify after deletion
+const checkAndDelete = (
+  db: DatabaseType,
+  selectQuery: string,
+  deleteQuery: string,
+  params: [Protocol, string] | [],
+) => {
+  // Count rows before deletion
+  const rowsBefore = db.prepare(selectQuery).all(...params)
+  console.log(`Rows to be deleted (${rowsBefore.length}):`)
+  console.table(rowsBefore)
+
+  // Perform the deletion
+  db.prepare(deleteQuery).run(...params)
+
+  // Verify rows after deletion
+  const rowsAfter = db.prepare(selectQuery).all(...params)
+  console.log(`Rows remaining after deletion (${rowsAfter.length}):`)
+  console.table(rowsAfter)
+}
