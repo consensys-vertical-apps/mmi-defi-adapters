1d2af4a83f90805d93793c9f4475037b2b809f12
Bernardo Garces Chapero <bernardo.chapero@consensys.net>
2025-01-24 09:55:55 +0000

    concurrent

--- packages/adapters-library/src/scripts/addressContractCacheBuilder/buildHistoricCache.ts
+++ packages/adapters-library/src/scripts/addressContractCacheBuilder/buildHistoricCache.ts
@@ -10,8 +10,9 @@ import {
 } from './db-queries'
 import { fetchEvents } from './fetchEvents'
 import { getAddress, JsonRpcProvider, Network } from 'ethers'
+import PQueue from 'p-queue'
 
-const POOL_BATCH_SIZE = 100
+const POOL_BATCH_SIZE = 50
 
 export async function buildHistoricCache(
   defiProvider: DefiProvider,
@@ -33,6 +34,8 @@ export async function buildHistoricCache(
     await insertContractEntries(defiProvider, chainId, db)
   }
 
+  const queue = new PQueue({ concurrency: 5 })
+
   while (true) {
     const pendingPools = fetchNextPoolsToProcess(db)
 
@@ -50,63 +53,99 @@ export async function buildHistoricCache(
       targetBlockNumber,
     })
 
-    let poolsAlreadyProcessed = 0
+    const poolBatches = []
     for (let i = 0; i < poolAddresses.length; i += POOL_BATCH_SIZE) {
       const contractAddresses = poolAddresses.slice(i, i + POOL_BATCH_SIZE)
-      console.log(`${new Date().toISOString()}: Processing batch`, {
-        batchIndex: i / POOL_BATCH_SIZE + 1,
-        totalBatches: Math.ceil(poolAddresses.length / POOL_BATCH_SIZE),
-        batchSize: contractAddresses.length,
-        totalPools: poolAddresses.length,
-        poolsAlreadyProcessed,
-      })
 
-      let logsAlreadyProcessed = 0
-      for await (const { logs, fromBlock, toBlock, depth } of fetchEvents({
-        provider,
-        contractAddresses,
-        topics: [null, null, null, null],
-        fromBlock: 0,
-        toBlock: targetBlockNumber,
-      })) {
-        logsAlreadyProcessed += logs.length
-        console.log(`${new Date().toISOString()}: Logs fetched`, {
-          logs: logs.length,
-          fromBlock,
-          toBlock,
-          depth,
-          logsAlreadyProcessed,
-        })
+      poolBatches.push(
+        queue.add(async () => {
+          console.log(`${new Date().toISOString()}: Pools batch started`, {
+            batchIndex: i / POOL_BATCH_SIZE + 1,
+            totalBatches: Math.ceil(poolAddresses.length / POOL_BATCH_SIZE),
+            batchSize: contractAddresses.length,
+            totalPools: poolAddresses.length,
+          })
 
-        const logsToInsert: [string, string][] = []
-        for (const log of logs) {
-          const contractAddress = getAddress(log.address.toLowerCase()).slice(2)
+          for await (const { logs, fromBlock, toBlock, depth } of fetchEvents({
+            provider,
+            contractAddresses,
+            topics: [null, null, null, null],
+            fromBlock: 0,
+            toBlock: targetBlockNumber,
+          })) {
+            // console.log(`${new Date().toISOString()}: Logs fetched`, {
+            //   logs: logs.length,
+            //   fromBlock,
+            //   toBlock,
+            //   depth,
+            //   batchIndex: i / POOL_BATCH_SIZE + 1,
+            // })
 
-          for (const topic of log.topics) {
-            if (
-              topic.startsWith('0x000000000000000000000000') && // Not an address if it is does not start with 0x000000000000000000000000
-              topic !==
-                '0x0000000000000000000000000000000000000000000000000000000000000000' // Skip the zero address
-            ) {
-              const topicAddress = getAddress(
-                `0x${topic.slice(-40).toLowerCase()}`,
+            const logsToInsert: [string, string][] = []
+            for (const log of logs) {
+              const contractAddress = getAddress(
+                log.address.toLowerCase(),
               ).slice(2)
 
-              logsToInsert.push([contractAddress, topicAddress])
+              for (const topic of log.topics) {
+                if (
+                  topic.startsWith('0x000000000000000000000000') && // Not an address if it is does not start with 0x000000000000000000000000
+                  topic !==
+                    '0x0000000000000000000000000000000000000000000000000000000000000000' // Skip the zero address
+                ) {
+                  const topicAddress = getAddress(
+                    `0x${topic.slice(-40).toLowerCase()}`,
+                  ).slice(2)
+
+                  logsToInsert.push([contractAddress, topicAddress])
+                }
+              }
             }
+
+            insertLogs(db, logsToInsert)
           }
-        }
 
-        insertLogs(db, logsToInsert)
+          completeJobs(db, contractAddresses)
 
-        logsAlreadyProcessed += logs.length
-      }
+          console.log(`${new Date().toISOString()}: Pools batch ended`, {
+            batchIndex: i / POOL_BATCH_SIZE + 1,
+            totalBatches: Math.ceil(poolAddresses.length / POOL_BATCH_SIZE),
+            batchSize: contractAddresses.length,
+            totalPools: poolAddresses.length,
+          })
 
-      completeJobs(db, contractAddresses)
+          logMemoryUsage()
 
-      poolsAlreadyProcessed += contractAddresses.length
+          // Hint to garbage collector
+          if (global.gc) {
+            global.gc()
+          }
+        }),
+      )
     }
 
-    await new Promise((resolve) => setTimeout(resolve, 5000))
+    const results = await Promise.allSettled(poolBatches)
+
+    console.log(`${new Date().toISOString()}: Loop completed`, {
+      succesfulBatches: results.filter(
+        (result) => result.status === 'fulfilled',
+      ).length,
+      failedBatches: results.filter((result) => result.status === 'rejected')
+        .length,
+      totalPools: poolAddresses.length,
+    })
+
+    await new Promise((resolve) => setTimeout(resolve, 10000))
   }
+}
+
+function logMemoryUsage() {
+  const used = process.memoryUsage()
+  const memoryUsageLog = Object.entries(used).reduce(
+    (acc, [key, value]) =>
+      `${acc} [${key}: ${Math.round(value / 1024 / 1024)} MB]`,
+    'Memory usage:',
+  )
+
+  console.log(memoryUsageLog)
 }
