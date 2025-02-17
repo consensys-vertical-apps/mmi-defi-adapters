a1d7daeb36357079fcbe4f4b4c2478852d7cc272
Bernardo Garces Chapero <bernardo.chapero@consensys.net>
2025-02-17 12:16:54 +0000

    correct endpoints for api

--- packages/adapters-api/src/logger.ts
+++ packages/adapters-api/src/logger.ts
@@ -1,7 +1,7 @@
 import { pino } from 'pino'
 
 export const logger = pino({
-  base: { service: 'defi-positions-workers' },
+  base: { service: 'defi-positions-api' },
   transport:
     process.env.LOG_PRETTY === 'true' ? { target: 'pino-pretty' } : undefined,
   level: process.env.LOG_LEVEL || 'info',
