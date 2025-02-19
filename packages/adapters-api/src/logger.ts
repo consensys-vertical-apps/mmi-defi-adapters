9215f562d13950e205525548c4698e4430b53b18
Bernardo Garces Chapero <bernardo.chapero@consensys.net>
2025-02-19 09:26:16 +0000

    api filters

---
+++ packages/adapters-api/src/logger.ts
@@ -0,0 +1,8 @@
+import { pino } from 'pino'
+
+export const logger = pino({
+  base: { service: 'defi-positions-api' },
+  transport:
+    process.env.LOG_PRETTY === 'true' ? { target: 'pino-pretty' } : undefined,
+  level: process.env.LOG_LEVEL || 'info',
+})
