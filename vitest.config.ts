af363047687e668c138be56243c6e29571a9c0b5
Bernardo Garces Chapero <bernardo.chapero@consensys.net>
2025-03-27 10:15:40 +0000

    add all projects to tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,11 +6,17 @@ export default defineConfig({
     workspace: [
       {
         test: {
-          name: 'adapters-unit',
-          include: ['packages/adapters-library/**/*.test.ts'],
+          name: 'unit',
+          include: [
+            'packages/adapters-library/**/*.test.ts',
+            'packages/adapters-api/**/*.test.ts',
+            'packages/adapters-cli/**/*.test.ts',
+            'packages/workers/**/*.test.ts',
+          ],
           exclude: [
             'packages/adapters-library/src/adapters/snapshots.test.ts',
-            'packages/adapters-library/src/tests/*.test.ts',
+            'packages/adapters-library/src/tests/smoke.test.ts',
+            'packages/adapters-cli/src/tests/new-adapter-templates.test.ts',
           ],
         },
       },
