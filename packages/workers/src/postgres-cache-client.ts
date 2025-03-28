e94e1ed131744fd1d4f9ec5715a2e1ce7a911998
Bernardo Garces Chapero <bernardo.chapero@consensys.net>
2025-03-28 12:46:06 +0000

    revert changes

--- packages/workers/src/postgres-cache-client.ts
+++ packages/workers/src/postgres-cache-client.ts
@@ -64,7 +64,7 @@ export async function createPostgresCacheClient({
     partialPoolConfig,
   })
 
-  await dbPool.query('CREATE SCHEMA IF NOT EXISTS $1', [schema])
+  await dbPool.query(`CREATE SCHEMA IF NOT EXISTS "${schema}"`)
 
   const client = new PostgresCacheClient(dbPool, chainId)
   await client.migrate()
