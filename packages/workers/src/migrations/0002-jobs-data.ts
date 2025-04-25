2e77dc991210e0498ffbe1cf3f2c99e04a910efb
Bernardo Garces Chapero <bernardo.chapero@consensys.net>
2025-04-25 12:32:40 +0100

    fix

--- packages/workers/src/migrations/0002-jobs-data.ts
+++ packages/workers/src/migrations/0002-jobs-data.ts
@@ -3,7 +3,7 @@ import type { Pool } from 'pg'
 export default async function (client: Pool) {
   await client.query(
     `ALTER TABLE jobs
-     ALTER COLUMN user_address_index DROP CONSTRAINT jobs_user_address_index_check,
+     DROP CONSTRAINT IF EXISTS jobs_user_address_index_check,
      ADD COLUMN event_abi TEXT`,
   )
 }
