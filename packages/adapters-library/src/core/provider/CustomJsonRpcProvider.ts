daf9edc0922676a4f32958a3b7ee2090ef16d6f8
Bernardo Garces Chapero <bernardo.chapero@consensys.net>
2025-04-28 16:31:29 +0100

    disable rpc cache by default

--- packages/adapters-library/src/core/provider/CustomJsonRpcProvider.ts
+++ packages/adapters-library/src/core/provider/CustomJsonRpcProvider.ts
@@ -19,6 +19,7 @@ export type CustomJsonRpcProviderOptions = {
   rpcCallRetries: number
   rpcGetLogsTimeoutInMs: number
   rpcGetLogsRetries: number
+  enableCache: boolean
 }
 const THIRTY_MINUTES = 30 * 60 * 1000
 
@@ -29,6 +30,7 @@ export class CustomJsonRpcProvider extends JsonRpcProvider {
   chainId: EvmChain
 
   private _hasUnlimitedGetLogsRange: boolean
+  private _enableCache: boolean
 
   private cacheCalls: Record<string, Promise<CacheEntryCalls>>
   private cacheLogs: Record<string, Promise<CacheEntryLogs>>
@@ -45,9 +47,9 @@ export class CustomJsonRpcProvider extends JsonRpcProvider {
       rpcCallRetries,
       rpcGetLogsTimeoutInMs,
       rpcGetLogsRetries,
+      enableCache,
     },
     jsonRpcProviderOptions,
-
     hasUnlimitedGetLogsRange,
   }: {
     fetchRequest: FetchRequest
@@ -70,6 +72,7 @@ export class CustomJsonRpcProvider extends JsonRpcProvider {
     this.cacheLogs = {}
 
     this._hasUnlimitedGetLogsRange = hasUnlimitedGetLogsRange
+    this._enableCache = enableCache
   }
 
   /**
@@ -94,6 +97,19 @@ export class CustomJsonRpcProvider extends JsonRpcProvider {
   }
 
   async call(transaction: TransactionRequest): Promise<string> {
+    const callHandler = async () => {
+      const result = this.callRetryHandler(() => super.call(transaction))
+
+      return {
+        result: await result,
+        timestamp: Date.now(),
+      }
+    }
+
+    if (!this._enableCache) {
+      return (await callHandler()).result
+    }
+
     const key = JSON.stringify({
       ...transaction,
       chainId: transaction.chainId?.toString(), // JSON.stringify cannot serialize bigints by default
@@ -111,14 +127,7 @@ export class CustomJsonRpcProvider extends JsonRpcProvider {
       }
     }
 
-    const entryPromise = (async () => {
-      const result = this.callRetryHandler(() => super.call(transaction))
-
-      return {
-        result: await result,
-        timestamp: Date.now(),
-      }
-    })()
+    const entryPromise = callHandler()
 
     this.cacheCalls[key] = entryPromise
 
@@ -126,6 +135,19 @@ export class CustomJsonRpcProvider extends JsonRpcProvider {
   }
 
   async getLogs(filter: Filter | FilterByBlockHash): Promise<Array<Log>> {
+    const getLogsHandler = async () => {
+      const result = this.logsRetryHandler(() => super.getLogs(filter))
+
+      return {
+        result: await result,
+        timestamp: Date.now(),
+      }
+    }
+
+    if (!this._enableCache) {
+      return (await getLogsHandler()).result
+    }
+
     const startTime = Date.now()
     const key = JSON.stringify(filter)
 
@@ -141,14 +163,7 @@ export class CustomJsonRpcProvider extends JsonRpcProvider {
       }
     }
 
-    const entryPromise = (async () => {
-      const result = this.logsRetryHandler(() => super.getLogs(filter))
-
-      return {
-        result: await result,
-        timestamp: Date.now(),
-      }
-    })()
+    const entryPromise = getLogsHandler()
 
     this.cacheLogs[key] = entryPromise
 
